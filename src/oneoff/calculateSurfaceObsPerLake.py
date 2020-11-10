b7c97a922acb74a2eda3451946000aa422c55533
Jared W <willa099@umn.edu>
2020-11-10 15:41:05 -0600

    one-off folder

---
+++ src/oneoff/calculateSurfaceObsPerLake.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+ids = pd.read_csv('../../metadata/pball_site_ids.csv', header=None)
+ids = ids[0].values
+glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv")
+
+train_lakes = [re.search('nhdhr_(.*)', x).group(1) for x in np.unique(glm_all_f['target_id'].values)]
+test_lakes = ids[~np.isin(ids, train_lakes)]
+
+pdb.set_trace()
+
+
