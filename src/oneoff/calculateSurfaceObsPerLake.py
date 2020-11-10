a225571bd196552302fc09d59efb926d53fd6ee7
Jared W <willa099@umn.edu>
2020-11-10 15:43:03 -0600

    one-off folder

--- src/oneoff/calculateSurfaceObsPerLake.py
+++ src/oneoff/calculateSurfaceObsPerLake.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+metadata = pd.read_feather("../../metadata/lake_metadata.feather")
 ids = pd.read_csv('../../metadata/pball_site_ids.csv', header=None)
 ids = ids[0].values
 glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv")
