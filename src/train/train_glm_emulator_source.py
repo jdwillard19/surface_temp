fbd883fa786c8d7f96f745470f936cc3a1e0348a
Jared W <willa099@umn.edu>
2020-11-13 15:24:20 -0600

    move to remote

--- src/train/train_glm_emulator_source.py
+++ src/train/train_glm_emulator_source.py
@@ -92,280 +92,279 @@ data_dir = "../../data/processed/"+lakename+"/"
 ##################################
 #create train and test sets
 
-
-for n_hidden in n_hidden_list:
-
-    if debug_eval:
-        break
-    #####################################################################################
-    ####################################################3
-    # fine tune
-    ###################################################33
-    ##########################################################################################33
-
-    #####################
-    #params
-    ###########################
-    first_save_epoch = 0
-    patience = 50
-    epoch_since_best = 0
-    lambda1 = 0.0001
-    data_dir = "../../data/processed/"+lakename+"/"
-    yhat_batch_size = 1
-
-    ###############################
-    # data preprocess
-    ##################################
-    #create train and test sets
-
-    (trn_data, all_data, all_phys_data, all_dates) = buildLakeDataForRNNPretrain(lakename, data_dir, seq_length, n_features,
-                                       win_shift= win_shift, begin_loss_ind=begin_loss_ind,
-                                       excludeTest=False, normAll=False, normGE10=False)
-    batch_size = trn_data.size()[0]
+if not debug_eval:
+    for n_hidden in n_hidden_list:
 
 
+        #####################################################################################
+        ####################################################3
+        # fine tune
+        ###################################################33
+        ##########################################################################################33
 
-    #Dataset classes
-    class TemperatureTrainDataset(Dataset):
-    #training dataset class, allows Dataloader to load both input/target
-        def __init__(self, trn_data):
-            self.len = trn_data.shape[0]
-            self.x_data = trn_data[:,:,:-1].float()
-            self.y_data = trn_data[:,:,-1].float()
+        #####################
+        #params
+        ###########################
+        first_save_epoch = 0
+        patience = 50
+        epoch_since_best = 0
+        lambda1 = 0.0001
+        data_dir = "../../data/processed/"+lakename+"/"
+        yhat_batch_size = 1
 
-        def __getitem__(self, index):
-            return self.x_data[index], self.y_data[index]
+        ###############################
+        # data preprocess
+        ##################################
+        #create train and test sets
 
-        def __len__(self):
-            return self.len
-
-    class TotalModelOutputDataset(Dataset):
-    #dataset for unsupervised input(in this case all the data)
-        def __init__(self, all_data, all_phys_data,all_dates):
-            #data of all model output, and corresponding unstandardized physical quantities
-            #needed to calculate physical loss
-            self.len = all_data.shape[0]
-            self.data = all_data[:,:,:-1].float()
-            self.label = all_data[:,:,-1].float() #DO NOT USE IN MODEL
-            self.phys = all_phys_data.float()
-            helper = np.vectorize(lambda x: date.toordinal(pd.Timestamp(x).to_pydatetime()))
-            dates = helper(all_dates)
-            self.dates = dates
-
-        def __getitem__(self, index):
-            return self.data[index], self.phys[index], self.dates[index], self.label[index]
-
-        def __len__(self):
-            return self.len
+        (trn_data, all_data, all_phys_data, all_dates) = buildLakeDataForRNNPretrain(lakename, data_dir, seq_length, n_features,
+                                           win_shift= win_shift, begin_loss_ind=begin_loss_ind,
+                                           excludeTest=False, normAll=False, normGE10=False)
+        batch_size = trn_data.size()[0]
 
 
 
+        #Dataset classes
+        class TemperatureTrainDataset(Dataset):
+        #training dataset class, allows Dataloader to load both input/target
+            def __init__(self, trn_data):
+                self.len = trn_data.shape[0]
+                self.x_data = trn_data[:,:,:-1].float()
+                self.y_data = trn_data[:,:,-1].float()
 
-    #format training data for loading
-    train_data = TemperatureTrainDataset(trn_data)
+            def __getitem__(self, index):
+                return self.x_data[index], self.y_data[index]
 
+            def __len__(self):
+                return self.len
 
-    #format total y-hat data for loading
-    total_data = TotalModelOutputDataset(all_data, all_phys_data, all_dates)
-    n_batches = math.floor(trn_data.size()[0] / batch_size)
+        class TotalModelOutputDataset(Dataset):
+        #dataset for unsupervised input(in this case all the data)
+            def __init__(self, all_data, all_phys_data,all_dates):
+                #data of all model output, and corresponding unstandardized physical quantities
+                #needed to calculate physical loss
+                self.len = all_data.shape[0]
+                self.data = all_data[:,:,:-1].float()
+                self.label = all_data[:,:,-1].float() #DO NOT USE IN MODEL
+                self.phys = all_phys_data.float()
+                helper = np.vectorize(lambda x: date.toordinal(pd.Timestamp(x).to_pydatetime()))
+                dates = helper(all_dates)
+                self.dates = dates
 
-    #batch samplers used to draw samples in dataloaders
-    batch_sampler = pytorch_data_operations.ContiguousBatchSampler(batch_size, n_batches)
+            def __getitem__(self, index):
+                return self.data[index], self.phys[index], self.dates[index], self.label[index]
 
-
-    #load val/test data into enumerator based on batch size
-    # testloader = torch.utils.data.DataLoader(tst_data, batch_size=tst_data.size()[0], shuffle=False, pin_memory=True)
-
-
-    #define LSTM model class
-    class myLSTM_Net(nn.Module):
-        def __init__(self, input_size, hidden_size, batch_size):
-            super(myLSTM_Net, self).__init__()
-            self.input_size = input_size
-            self.hidden_size = hidden_size
-            self.batch_size = batch_size
-            self.lstm = nn.LSTM(input_size = n_features, hidden_size=hidden_size, batch_first=True,num_layers=num_layers) #batch_first=True?
-            self.out = nn.Linear(hidden_size, 1) #1?
-            self.hidden = self.init_hidden()
-            self.w_upper_to_lower = []
-            self.w_lower_to_upper = []
-
-        def init_hidden(self, batch_size=0):
-            # initialize both hidden layers
-            if batch_size == 0:
-                batch_size = self.batch_size
-            ret = (xavier_normal_(torch.empty(1, batch_size, self.hidden_size)),
-                    xavier_normal_(torch.empty(1, batch_size, self.hidden_size)))
-            if use_gpu:
-                item0 = ret[0].cuda(non_blocking=True)
-                item1 = ret[1].cuda(non_blocking=True)
-                ret = (item0,item1)
-            return ret
-
-        def forward(self, x, hidden):
-            self.lstm.flatten_parameters()
-            x = x.float()
-            x, hidden = self.lstm(x, self.hidden)
-            self.hidden = hidden
-            x = self.out(x)
-            return x, hidden
-
-    #method to calculate l1 norm of model
-    def calculate_l1_loss(model):
-        def l1_loss(x):
-            return torch.abs(x).sum()
-
-        to_regularize = []
-        # for name, p in model.named_parameters():
-        for name, p in model.named_parameters():
-            if 'bias' in name:
-                continue
-            else:
-                #take absolute value of weights and sum
-                to_regularize.append(p.view(-1))
-        l1_loss_val = torch.tensor(1, requires_grad=True, dtype=torch.float32)
-        l1_loss_val = l1_loss(torch.cat(to_regularize))
-        return l1_loss_val
-
-
-    lstm_net = myLSTM_Net(n_features, n_hidden, batch_size)
-
-    #tell model to use GPU if needed
-    if use_gpu:
-        lstm_net = lstm_net.cuda()
+            def __len__(self):
+                return self.len
 
 
 
 
-    #define loss and optimizer
-    mse_criterion = nn.MSELoss()
-    optimizer = optim.Adam(lstm_net.parameters(), lr=.001)#, weight_decay=0.01)
+        #format training data for loading
+        train_data = TemperatureTrainDataset(trn_data)
 
-    #training loop
 
-    min_mse = 99999
-    min_mse_tsterr = None
-    ep_min_mse = -1
-    ep_since_min = 0
-    best_pred_mat = np.empty(())
-    manualSeed = [random.randint(1, 99999999) for i in range(n_eps)]
+        #format total y-hat data for loading
+        total_data = TotalModelOutputDataset(all_data, all_phys_data, all_dates)
+        n_batches = math.floor(trn_data.size()[0] / batch_size)
 
-    #stop training if true
-    done = False
-
-    for epoch in range(n_eps):
-        if verbose:
-            print("train epoch: ", epoch)
-
-        if done:
-            break
-
-        running_loss = 0.0
-
-        #reload loader for shuffle
         #batch samplers used to draw samples in dataloaders
         batch_sampler = pytorch_data_operations.ContiguousBatchSampler(batch_size, n_batches)
-        batch_sampler_all = pytorch_data_operations.RandomContiguousBatchSampler(all_data.size()[0], seq_length, yhat_batch_size, n_batches)
-
-        alldataloader = DataLoader(total_data, batch_sampler=batch_sampler_all, pin_memory=True)
-        trainloader = DataLoader(train_data, batch_sampler=batch_sampler, pin_memory=True)
-        multi_loader = pytorch_data_operations.MultiLoader([trainloader, alldataloader])
 
 
-        #zero the parameter gradients
-        optimizer.zero_grad()
-        lstm_net.train(True)
-        avg_loss = 0
-        batches_done = 0
-        for i, batches in enumerate(multi_loader):
-            #load data
-            inputs = None
-            targets = None
-            for j, b in enumerate(batches):
-                if j == 0:
-                    inputs, targets = b
+        #load val/test data into enumerator based on batch size
+        # testloader = torch.utils.data.DataLoader(tst_data, batch_size=tst_data.size()[0], shuffle=False, pin_memory=True)
 
 
-            #cuda commands
-            if(use_gpu):
-                inputs = inputs.cuda()
-                targets = targets.cuda()
+        #define LSTM model class
+        class myLSTM_Net(nn.Module):
+            def __init__(self, input_size, hidden_size, batch_size):
+                super(myLSTM_Net, self).__init__()
+                self.input_size = input_size
+                self.hidden_size = hidden_size
+                self.batch_size = batch_size
+                self.lstm = nn.LSTM(input_size = n_features, hidden_size=hidden_size, batch_first=True,num_layers=num_layers) #batch_first=True?
+                self.out = nn.Linear(hidden_size, 1) #1?
+                self.hidden = self.init_hidden()
+                self.w_upper_to_lower = []
+                self.w_lower_to_upper = []
 
-            #forward  prop
-            lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
-            h_state = None
-            outputs, h_state = lstm_net(inputs, h_state)
-            outputs = outputs.view(outputs.size()[0],-1)
+            def init_hidden(self, batch_size=0):
+                # initialize both hidden layers
+                if batch_size == 0:
+                    batch_size = self.batch_size
+                ret = (xavier_normal_(torch.empty(1, batch_size, self.hidden_size)),
+                        xavier_normal_(torch.empty(1, batch_size, self.hidden_size)))
+                if use_gpu:
+                    item0 = ret[0].cuda(non_blocking=True)
+                    item1 = ret[1].cuda(non_blocking=True)
+                    ret = (item0,item1)
+                return ret
 
-            #calculate losses
-            reg1_loss = 0
-            if lambda1 > 0:
-                reg1_loss = calculate_l1_loss(lstm_net)
+            def forward(self, x, hidden):
+                self.lstm.flatten_parameters()
+                x = x.float()
+                x, hidden = self.lstm(x, self.hidden)
+                self.hidden = hidden
+                x = self.out(x)
+                return x, hidden
+
+        #method to calculate l1 norm of model
+        def calculate_l1_loss(model):
+            def l1_loss(x):
+                return torch.abs(x).sum()
+
+            to_regularize = []
+            # for name, p in model.named_parameters():
+            for name, p in model.named_parameters():
+                if 'bias' in name:
+                    continue
+                else:
+                    #take absolute value of weights and sum
+                    to_regularize.append(p.view(-1))
+            l1_loss_val = torch.tensor(1, requires_grad=True, dtype=torch.float32)
+            l1_loss_val = l1_loss(torch.cat(to_regularize))
+            return l1_loss_val
 
 
-            loss_outputs = outputs[:,begin_loss_ind:]
-            loss_targets = targets[:,begin_loss_ind:].cpu()
+        lstm_net = myLSTM_Net(n_features, n_hidden, batch_size)
+
+        #tell model to use GPU if needed
+        if use_gpu:
+            lstm_net = lstm_net.cuda()
 
 
-            #get indices to calculate loss
-            loss_indices = np.array(np.isfinite(loss_targets.cpu()), dtype='bool_')
 
-            if use_gpu:
-                loss_outputs = loss_outputs.cuda()
-                loss_targets = loss_targets.cuda()
-            loss = mse_criterion(loss_outputs[loss_indices], loss_targets[loss_indices]) + lambda1*reg1_loss 
-            #backward
 
-            loss.backward(retain_graph=False)
-            if grad_clip > 0:
-                clip_grad_norm_(lstm_net.parameters(), grad_clip, norm_type=2)
+        #define loss and optimizer
+        mse_criterion = nn.MSELoss()
+        optimizer = optim.Adam(lstm_net.parameters(), lr=.001)#, weight_decay=0.01)
 
-            #optimize
-            optimizer.step()
+        #training loop
+
+        min_mse = 99999
+        min_mse_tsterr = None
+        ep_min_mse = -1
+        ep_since_min = 0
+        best_pred_mat = np.empty(())
+        manualSeed = [random.randint(1, 99999999) for i in range(n_eps)]
+
+        #stop training if true
+        done = False
+
+        for epoch in range(n_eps):
+            if verbose:
+                print("train epoch: ", epoch)
+
+            if done:
+                break
+
+            running_loss = 0.0
+
+            #reload loader for shuffle
+            #batch samplers used to draw samples in dataloaders
+            batch_sampler = pytorch_data_operations.ContiguousBatchSampler(batch_size, n_batches)
+            batch_sampler_all = pytorch_data_operations.RandomContiguousBatchSampler(all_data.size()[0], seq_length, yhat_batch_size, n_batches)
+
+            alldataloader = DataLoader(total_data, batch_sampler=batch_sampler_all, pin_memory=True)
+            trainloader = DataLoader(train_data, batch_sampler=batch_sampler, pin_memory=True)
+            multi_loader = pytorch_data_operations.MultiLoader([trainloader, alldataloader])
+
 
             #zero the parameter gradients
             optimizer.zero_grad()
-            avg_loss += loss
-            batches_done += 1
-
-        #check for convergence
-        avg_loss = avg_loss / batches_done
-        if verbose:
-            print("rmse loss=", avg_loss)
-
-        if avg_loss < min_mse:
-            min_mse = avg_loss
-            ep_min_mse = epoch
-            ep_since_min = 0
-
-        else:
-            ep_since_min += 1
-
-        if ep_since_min == patience:
-            print("patience met")
-            done = True
-            break
+            lstm_net.train(True)
+            avg_loss = 0
+            batches_done = 0
+            for i, batches in enumerate(multi_loader):
+                #load data
+                inputs = None
+                targets = None
+                for j, b in enumerate(batches):
+                    if j == 0:
+                        inputs, targets = b
 
 
+                #cuda commands
+                if(use_gpu):
+                    inputs = inputs.cuda()
+                    targets = targets.cuda()
+
+                #forward  prop
+                lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
+                h_state = None
+                outputs, h_state = lstm_net(inputs, h_state)
+                outputs = outputs.view(outputs.size()[0],-1)
+
+                #calculate losses
+                reg1_loss = 0
+                if lambda1 > 0:
+                    reg1_loss = calculate_l1_loss(lstm_net)
 
 
-        if epoch % 100 == 0 and epoch != 0:
-
-            save_path = "../../models/"+lakename+"/LSTM_source_model_"+str(n_hidden)+"hid_"+str(epoch)+"ep"
-
-            saveModel(lstm_net.state_dict(), optimizer.state_dict(), save_path)
-            if n_hidden == 20:
-                ep_list20.append(epoch)
-            elif n_hidden == 50:
-                ep_list50.append(epoch)
-
-            print("saved at ",save_path)
+                loss_outputs = outputs[:,begin_loss_ind:]
+                loss_targets = targets[:,begin_loss_ind:].cpu()
 
 
-            break
+                #get indices to calculate loss
+                loss_indices = np.array(np.isfinite(loss_targets.cpu()), dtype='bool_')
+
+                if use_gpu:
+                    loss_outputs = loss_outputs.cuda()
+                    loss_targets = loss_targets.cuda()
+                loss = mse_criterion(loss_outputs[loss_indices], loss_targets[loss_indices]) + lambda1*reg1_loss 
+                #backward
+
+                loss.backward(retain_graph=False)
+                if grad_clip > 0:
+                    clip_grad_norm_(lstm_net.parameters(), grad_clip, norm_type=2)
+
+                #optimize
+                optimizer.step()
+
+                #zero the parameter gradients
+                optimizer.zero_grad()
+                avg_loss += loss
+                batches_done += 1
+
+            #check for convergence
+            avg_loss = avg_loss / batches_done
+            if verbose:
+                print("rmse loss=", avg_loss)
+
+            if avg_loss < min_mse:
+                min_mse = avg_loss
+                ep_min_mse = epoch
+                ep_since_min = 0
+
+            else:
+                ep_since_min += 1
+
+            if ep_since_min == patience:
+                print("patience met")
+                done = True
+                break
 
 
-print("|\n|\nTraining Candidate Models Complete\n|\n|")
+
+
+            if epoch % 100 == 0 and epoch != 0:
+
+                save_path = "../../models/"+lakename+"/LSTM_source_model_"+str(n_hidden)+"hid_"+str(epoch)+"ep"
+
+                saveModel(lstm_net.state_dict(), optimizer.state_dict(), save_path)
+                if n_hidden == 20:
+                    ep_list20.append(epoch)
+                elif n_hidden == 50:
+                    ep_list50.append(epoch)
+
+                print("saved at ",save_path)
+
+
+                break
+
+
+    print("|\n|\nTraining Candidate Models Complete\n|\n|")
 ##################################################################
 # transfer all models to all other source lakes to find best one
 ########################################################################
