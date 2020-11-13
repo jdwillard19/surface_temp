f8b7138804c6f4df0804adf892113a79460ffae1
Jared W <willa099@umn.edu>
2020-11-13 15:32:06 -0600

    move to remote

--- src/train/train_glm_emulator_source.py
+++ src/train/train_glm_emulator_source.py
@@ -372,7 +372,6 @@ glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv"
 train_lakes = np.array([re.search('nhdhr_(.*)', x).group(1) for x in np.unique(glm_all_f['target_id'].values)])
 
 other_source_ids = train_lakes[~np.isin(train_lakes,site_id)] #remove site id
-pdb.set_trace()
 other_source_ids = other_source_ids[~np.isin(other_source_ids, '121623043')] #remove cuz only 1 surf temp obs
 
 err_per_epoch20 = np.empty((len(ep_list20)))
