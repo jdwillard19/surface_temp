91489b0d6a1fe15700ed52fecea7e512276748e0
Jared W <willa099@umn.edu>
2020-11-13 15:42:03 -0600

    move to remote

--- src/train/train_glm_emulator_source.py
+++ src/train/train_glm_emulator_source.py
@@ -372,7 +372,9 @@ glm_all_f = pd.read_csv("../../results/glm_transfer/RMSE_transfer_glm_pball.csv"
 train_lakes = np.array([re.search('nhdhr_(.*)', x).group(1) for x in np.unique(glm_all_f['target_id'].values)])
 
 other_source_ids = train_lakes[~np.isin(train_lakes,site_id)] #remove site id
-other_source_ids = other_source_ids[~np.isin(other_source_ids, ['121623043','121623126','121860894','143249413'])] #remove cuz only 1,0,0,0 surf temp obs
+other_source_ids = other_source_ids[~np.isin(other_source_ids, ['121623043','121623126',\
+                                                                '121860894','143249413',\
+                                                                '143249864'])] #remove cuz only 1,0,0,0,1 surf temp obs
 
 
 err_per_epoch20 = np.empty((len(ep_list20)))
