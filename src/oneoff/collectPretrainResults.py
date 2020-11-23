c8896ee6e6a112aa80a8cbc4b565aa1882211dc0
Jared W <willa099@umn.edu>
2020-11-23 13:06:04 -0600

    move to remote

--- src/oneoff/collectPretrainResults.py
+++ src/oneoff/collectPretrainResults.py
@@ -5,15 +5,39 @@ import pdb
 
 
 train_ids = np.load("../../data/static/lists/source_lakes_wrr.npy")
-
+n_lakes = train_lakes.shape[0]
 job_base_path = "../hpc/jobs/train_glm_emulator_"
+
+err_per_glm16 = np.empty((n_lakes))
+err_per_glm16[:] = np.nan
+
+err_per_glm32 = np.empty((n_lakes))
+err_per_glm32[:] = np.nan
+
+err_per_glm64 = np.empty((n_lakes))
+err_per_glm64[:] = np.nan
+
+err_per_glm128 = np.empty((n_lakes))
+err_per_glm128[:] = np.nan
 
 def job_path(site_id):
 	return "../hpc/jobs/train_glm_emulator_"+str(site_id)+".out"
-for site_id in train_ids:
+for site_ct, site_id in enumerate(train_ids):
+	print("starting lake #",site_ct)
 	#load result
     out_f = open(job_path(site_id), "r")
     out_txt = out_f.read()
-    match16 = re.findall(pattern="BEST_MODEL_PATH_16HID:../../models/105954753/LSTM_source_model_16hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
-    pdb.set_trace()
-    hid16rmse = match16[2]
+    match16 = re.findall(pattern="BEST_MODEL_PATH_16HID:../../models/"+str(site_id)+"/LSTM_source_model_16hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    err_per_glm16[site_ct] = match16[0][1]
+
+    match32 = re.findall(pattern="BEST_MODEL_PATH_32HID:../../models/"+str(site_id)+"/LSTM_source_model_32hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    err_per_glm32[site_ct] = match32[0][1]
+
+    match64 = re.findall(pattern="BEST_MODEL_PATH_64HID:../../models/"+str(site_id)+"/LSTM_source_model_64hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    err_per_glm64[site_ct] = match64[0][1]
+
+    match128 = re.findall(pattern="BEST_MODEL_PATH_128HID:../../models/"+str(site_id)+"/LSTM_source_model_128hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    err_per_glm128[site_ct] = match128[0][1]
+
+
+pdb.set_trace()
