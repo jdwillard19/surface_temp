01380c7df40bdf9e6158d3cce7586479022c5399
Jared W <willa099@umn.edu>
2020-11-23 13:10:14 -0600

    move to remote

--- src/oneoff/collectPretrainResults.py
+++ src/oneoff/collectPretrainResults.py
@@ -27,16 +27,16 @@ for site_ct, site_id in enumerate(train_ids):
     #load result
     out_f = open(job_path(site_id), "r")
     out_txt = out_f.read()
-    match16 = re.findall(pattern="BEST_MODEL_PATH_16HID:../../models/"+str(site_id)+"/LSTM_source_model_16hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    match16 = re.findall(pattern="BEST_MODEL_PATH_16HID:../../models/105954753/LSTM_source_model_16hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
     err_per_glm16[site_ct] = match16[0][1]
 
-    match32 = re.findall(pattern="BEST_MODEL_PATH_32HID:../../models/"+str(site_id)+"/LSTM_source_model_32hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    match32 = re.findall(pattern="BEST_MODEL_PATH_32HID:../../models/105954753/LSTM_source_model_32hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
     err_per_glm32[site_ct] = match32[0][1]
 
-    match64 = re.findall(pattern="BEST_MODEL_PATH_64HID:../../models/"+str(site_id)+"/LSTM_source_model_64hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    match64 = re.findall(pattern="BEST_MODEL_PATH_64HID:../../models/105954753/LSTM_source_model_64hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
     err_per_glm64[site_ct] = match64[0][1]
 
-    match128 = re.findall(pattern="BEST_MODEL_PATH_128HID:../../models/"+str(site_id)+"/LSTM_source_model_128hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
+    match128 = re.findall(pattern="BEST_MODEL_PATH_128HID:../../models/105954753/LSTM_source_model_128hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=re.M | re.DOTALL)
     err_per_glm128[site_ct] = match128[0][1]
 
 
