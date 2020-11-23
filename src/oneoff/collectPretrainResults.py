cc349db78fefb9527409a1b0c93515833fde160f
Jared W <willa099@umn.edu>
2020-11-23 12:51:38 -0600

    move to remote

--- src/oneoff/collectPretrainResults.py
+++ src/oneoff/collectPretrainResults.py
@@ -12,8 +12,8 @@ def job_path(site_id):
 	return "../hpc/jobs/train_glm_emulator_"+str(site_id)+".out"
 for site_id in train_ids:
 	#load result
-    out_f = open(, "r")
-    out_txt = out_f.read(job_path(site_id))
+    out_f = open(job_path(site_id), "r")
+    out_txt = out_f.read(out_f)
     match16 = re.findall(pattern="BEST_MODEL_PATH_16HID:../../models/105954753/LSTM_source_model_16hid_(\d+)ep\nRMSE=(\d*\.?\d+)",string=out_txt,flags=flags=re.M | re.DOTALL)
     pdb.set_trace()
     hid16rmse = match16[2]
