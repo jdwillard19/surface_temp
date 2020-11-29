39b31e6b2a931e9a5f0daf10d9cc833d5a9d2ea7
Jared W <willa099@umn.edu>
2020-11-29 17:17:09 -0600

    move to remote

--- src/train/train_simple_global_LSTM.py
+++ src/train/train_simple_global_LSTM.py
@@ -68,9 +68,9 @@ save = True
 grad_clip = 1.0 #how much to clip the gradient 2-norm in training
 dropout = 0.
 num_layers = 2
-n_hidden = 16
+n_hidden = 32
 # lambda1 = 
-lambda1 = 0
+lambda1 = 1e-6
 
 n_eps = 10000
 
