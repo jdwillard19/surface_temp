8e810e53a91dbdf30be10c6d955c8583beb977ec
Jared W <willa099@umn.edu>
2020-11-29 15:14:12 -0600

    move to remote

--- src/train/train_simple_global_LSTM.py
+++ src/train/train_simple_global_LSTM.py
@@ -68,7 +68,7 @@ save = True
 grad_clip = 1.0 #how much to clip the gradient 2-norm in training
 dropout = 0
 num_layers = 2
-n_hidden = 16
+n_hidden = 8
 n_eps = 10000
 
 ep_list16 = [] #list of epochs at which models were saved for * hidden units
