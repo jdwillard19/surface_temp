7bb037a21f0924157ecfcdef16d72d766d091f27
Jared W <willa099@umn.edu>
2020-12-04 14:50:49 -0600

    move to remote

--- src/evaluate/predict_surface_temp_simple_global_LSTM.py
+++ src/evaluate/predict_surface_temp_simple_global_LSTM.py
@@ -24,17 +24,17 @@ test_lakes = np.load("../../data/static/lists/target_lakes_wrr.npy",allow_pickle
 # test_lakes = test_lakes[~np.isin(test_lakes, train_lakes)]
 # np.save("../../data/static/lists/target_lakes_wrr.npy",test_lakes)
 # print(len(test_lakes), "test lakes")
-remove_ct = 0
-for lake in test_lakes:
-    filter_df = obs_df[obs_df['site_id'] == "nhdhr_"+lake]
-    filter_df = filter_df[filter_df['depth'] <= .25]
-    if filter_df.shape[0] < 2:
-        test_lakes = np.delete(test_lakes,np.where(test_lakes == lake))
-        remove_ct += 1
+# remove_ct = 0
+# for lake in test_lakes:
+#     filter_df = obs_df[obs_df['site_id'] == "nhdhr_"+lake]
+#     filter_df = filter_df[filter_df['depth'] <= .25]
+#     if filter_df.shape[0] < 2:
+#         test_lakes = np.delete(test_lakes,np.where(test_lakes == lake))
+#         remove_ct += 1
 
-print(remove_ct, "removed")
-np.save("../../data/static/lists/target_lakes_wrr.npy",test_lakes)
-sys.exit()
+# print(remove_ct, "removed")
+# np.save("../../data/static/lists/target_lakes_wrr.npy",test_lakes)
+# sys.exit()
 # assert len(test_lakes) == 305
 
 k = 1
