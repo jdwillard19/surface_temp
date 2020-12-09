b552c86e99b5b9812b2298fb9ce39aea102268db
Jared W <willa099@umn.edu>
2020-12-09 16:01:55 -0600

    move to remote

--- src/metamodel/pgmtl_feature_selection.py
+++ src/metamodel/pgmtl_feature_selection.py
@@ -6,7 +6,8 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.feature_selection import RFECV
 import math
 import re
-
+import xgboost as xgb
+from sklearn.feature_selection import SelectFromModel
 
 ###################################################################################################
 # (Sept 2020 - Jared) - This script performs feature selection for PG-MTL metamodel
@@ -19,6 +20,7 @@ import re
 # train_lakes = [re.search('nhdhr_(.*)', x).group(1) for x in np.unique(glm_all_f['target_id'].values)]
 train_lakes = np.load("../../data/static/lists/source_lakes_wrr.npy")
 train_lakes_wp = ["nhdhr_"+x for x in train_lakes]
+test_lakes = np.load("../../data/static/lists/target_lakes_wrr.npy",allow_pickle=True)
 
 # train_lakes_wp = np.unique(glm_all_f['target_id'].values)
 n_lakes = len(train_lakes)
@@ -27,7 +29,9 @@ n_lakes = len(train_lakes)
 train_df = pd.DataFrame()
 
 feats = pd.read_feather("../../metadata/diffs/target_nhdhr_91685677.feather").columns[1:][75:]
-#compile all the meta-features and meta-target values into one dataframe
+
+
+#compile all the meta-features and meta-target values into one dataframe for training
 for _, lake_id in enumerate(train_lakes):
 
 	new_df = pd.DataFrame()
@@ -45,30 +49,47 @@ for _, lake_id in enumerate(train_lakes):
 	new_df = lake_df
 	train_df = pd.concat([train_df, new_df], ignore_index=True)
 
+test_df = pd.DataFrame()
 
-pdb.set_trace()
+for targ_ct, target_id in enumerate(test_lakes): #for each target lake
+    # print(str(targ_ct),'/',len(test_lakes),':',target_id)
+    lake_df = pd.DataFrame()
+    lake_id = target_id
+    lake_df = pd.read_feather("../../metadata/diffs/target_nhdhr_"+lake_id+".feather")
+    lake_df = lake_df[np.isin(lake_df['site_id'], train_lakes_wp)]
+    X = pd.DataFrame(lake_df[feats])
+    test_df = pd.concat([test_df, lake_df])
+
+
 #declare model and predictors and response
-est = GradientBoostingRegressor(n_estimators=800, learning_rate=0.1)
-X = pd.DataFrame(train_df[feats])
-y = np.ravel(pd.DataFrame(train_df['rmse']))
+# est = GradientBoostingRegressor(n_estimators=800, learning_rate=0.1)
+X_trn = pd.DataFrame(train_df[feats])
+X_tst = pd.DataFrame(test_df[feats])
+y_trn = np.ravel(pd.DataFrame(train_df['rmse']))
+# y_tst = np.ravel(pd.DataFrame(test_df['rmse']))
+dtrain = xgb.DMatrix(X_trn, label=y_trn)
 
 #perform recursive feature elimination
-rfecv = RFECV(estimator=est, cv=24, step=2, scoring='neg_mean_squared_error', verbose=1, n_jobs=-1)
-rfecv.fit(X, y)
+# rfecv = RFECV(estimator=est, cv=24, step=2, scoring='neg_mean_squared_error', verbose=1, n_jobs=-1)
+gbm = xgb.XGBRegressor().fit(X_train,y_train)
+selection = SelectFromModel(gbm, threshold=0.03, prefit=True)
+selected_dataset = selection.transform(X_test)
+# rfecv.fit(X, y)
 
-print("Optimal number of features : %d" % rfecv.n_features_)
+# print("Optimal number of features : %d" % rfecv.n_features_)
 
 # Plot number of features VS. cross-validation scores
-print("ranking: ", rfecv.ranking_)
+# print("ranking: ", rfecv.ranking_)
 
-print("scores: ", rfecv.grid_scores_)
+print("scores: ", selector.estimator_.coef_)
+print("support: ",selector.get_support())
+pdb.set_trace()
+# print("ranking: ", repr(rfecv.ranking_))
 
-print("ranking: ", repr(rfecv.ranking_))
-
-print("scores: ", repr(rfecv.grid_scores_))
+# print("scores: ", repr(rfecv.grid_scores_))
 
 print("selected features\n---------------------------------------------------------------------------")
-print(feats[rfecv.ranking_==1])
+print(feats[selector.get_support()])
 print("------------------------------------------------------------------------")
 
 
