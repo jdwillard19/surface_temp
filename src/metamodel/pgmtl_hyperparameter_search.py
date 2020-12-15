be8493a64480fe099b0918be7e26586daea38d1d
Jared W <willa099@umn.edu>
2020-12-15 16:03:03 -0600

    move to remote

--- src/metamodel/pgmtl_hyperparameter_search.py
+++ src/metamodel/pgmtl_hyperparameter_search.py
@@ -55,15 +55,28 @@ nfolds = 24
 #        'perc_dif_surface_area']
 
 #NO TRAN NO PRETRAIN
+# feats = ['n_obs', 'n_obs_sp', 'n_obs_su', 'n_obs_au', 'obs_temp_mean',
+#        'obs_temp_skew', 'obs_temp_kurt', 'obs_temp_mean_airdif',
+#        'dif_surface_area', 'dif_lw_std', 'dif_at_std', 'dif_snow_mean',
+#        'dif_rh_std_su', 'dif_snow_mean_su', 'dif_sw_mean_au', 'dif_lw_mean_au',
+#        'dif_lw_std_au', 'dif_at_std_au', 'dif_rh_std_au', 'dif_rain_mean_au',
+#        'dif_snow_mean_au', 'dif_lw_std_wi', 'dif_rain_mean_wi',
+#        'perc_dif_surface_area']
+
+#NO TRAN W PRETRAIN
 feats = ['n_obs', 'n_obs_sp', 'n_obs_su', 'n_obs_au', 'obs_temp_mean',
-       'obs_temp_skew', 'obs_temp_kurt', 'obs_temp_mean_airdif',
-       'dif_surface_area', 'dif_lw_std', 'dif_at_std', 'dif_snow_mean',
-       'dif_rh_std_su', 'dif_snow_mean_su', 'dif_sw_mean_au', 'dif_lw_mean_au',
-       'dif_lw_std_au', 'dif_at_std_au', 'dif_rh_std_au', 'dif_rain_mean_au',
-       'dif_snow_mean_au', 'dif_lw_std_wi', 'dif_rain_mean_wi',
-       'perc_dif_surface_area']
-
-
+       'obs_temp_std', 'obs_temp_skew', 'obs_temp_kurt', 'ad_zero_temp_doy',
+       'obs_temp_mean_airdif', 'dif_SDF', 'dif_k_d', 'dif_surface_area',
+       'dif_sw_mean', 'dif_lw_std', 'dif_at_std', 'dif_rh_mean', 'dif_rh_std',
+       'dif_rain_mean', 'dif_rain_std', 'dif_sw_std_sp', 'dif_at_mean_sp',
+       'dif_sw_mean_su', 'dif_sw_std_su', 'dif_lw_std_su', 'dif_rh_mean_su',
+       'dif_rh_std_su', 'dif_ws_mean_su', 'dif_rain_mean_su',
+       'dif_snow_mean_su', 'dif_snow_std_su', 'dif_sw_mean_au',
+       'dif_lw_std_au', 'dif_at_mean_au', 'dif_at_std_au', 'dif_rh_mean_au',
+       'dif_rh_std_au', 'dif_ws_mean_au', 'dif_rain_mean_au',
+       'dif_rain_std_au', 'dif_snow_mean_au', 'dif_sw_std_wi',
+       'dif_rh_mean_wi', 'dif_ws_mean_wi', 'dif_zero_temp_doy',
+       'dif_ws_sp_mix', 'perc_dif_surface_area', 'dif_sqrt_surface_area']
 
 
 # feats = ['n_obs', 'obs_temp_mean', 'dif_max_depth', 'dif_surface_area',
@@ -81,7 +94,7 @@ for _, lake_id in enumerate(train_lakes):
 
    #get performance results (metatargets), filter out target as source
     # lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results",header=None,names=['source_id','rmse'])
-    lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results_noTran_noPre",header=None,names=['source_id','rmse'])
+    lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results_noTran_wPre",header=None,names=['source_id','rmse'])
     lake_df_res = lake_df_res[lake_df_res.source_id != 'source_id']
 
     #get metadata differences between target and all the sources
