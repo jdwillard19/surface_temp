9d37a4a30cb1d0d1e0df41f6aa2b0bfc561d13c2
Jared W <willa099@umn.edu>
2020-12-17 13:16:39 -0600

    move to remote

--- src/metamodel/pgmtl_train_metamodel.py
+++ src/metamodel/pgmtl_train_metamodel.py
@@ -20,15 +20,15 @@ currentDT = datetime.datetime.now()
 print("script start: ",str(currentDT))
 
 #file to save model  to
-# save_file_path = '../../models/metamodel_xgb_pgdl.joblib'
-save_file_path = '../../models/metamodel_xgb_noTran_wPre.joblib'
+save_file_path = '../../models/metamodel_xgb_pgdl2.joblib'
+# save_file_path = '../../models/metamodel_xgb_noTran_wPre.joblib'
 # save_file_path = '../../models/metamodel_pgdl_RMSE_GBR.joblib'
 
 #########################################################################################
 #paste features found in "pbmtl_feature_selection.py" here
-# feats = ['n_obs_sp', 'n_obs_su', 'dif_max_depth', 'dif_surface_area',
-#        'dif_glm_strat_perc', 'perc_dif_max_depth', 'perc_dif_surface_area',
-#        'perc_dif_sqrt_surface_area']
+feats = ['n_obs_sp', 'n_obs_su', 'dif_max_depth', 'dif_surface_area',
+       'dif_glm_strat_perc', 'perc_dif_max_depth', 'perc_dif_surface_area',
+       'perc_dif_sqrt_surface_area']
 
 
 #W TRANSFER OPTIM, W PRETRAIN
@@ -47,19 +47,19 @@ save_file_path = '../../models/metamodel_xgb_noTran_wPre.joblib'
 #        'perc_dif_surface_area']
 
 #NO TRAN W PRETRAIN
-feats = ['n_obs', 'n_obs_sp', 'n_obs_su', 'n_obs_au', 'obs_temp_mean',
-       'obs_temp_std', 'obs_temp_skew', 'obs_temp_kurt', 'ad_zero_temp_doy',
-       'obs_temp_mean_airdif', 'dif_SDF', 'dif_k_d', 'dif_surface_area',
-       'dif_sw_mean', 'dif_lw_std', 'dif_at_std', 'dif_rh_mean', 'dif_rh_std',
-       'dif_rain_mean', 'dif_rain_std', 'dif_sw_std_sp', 'dif_at_mean_sp',
-       'dif_sw_mean_su', 'dif_sw_std_su', 'dif_lw_std_su', 'dif_rh_mean_su',
-       'dif_rh_std_su', 'dif_ws_mean_su', 'dif_rain_mean_su',
-       'dif_snow_mean_su', 'dif_snow_std_su', 'dif_sw_mean_au',
-       'dif_lw_std_au', 'dif_at_mean_au', 'dif_at_std_au', 'dif_rh_mean_au',
-       'dif_rh_std_au', 'dif_ws_mean_au', 'dif_rain_mean_au',
-       'dif_rain_std_au', 'dif_snow_mean_au', 'dif_sw_std_wi',
-       'dif_rh_mean_wi', 'dif_ws_mean_wi', 'dif_zero_temp_doy',
-       'dif_ws_sp_mix', 'perc_dif_surface_area', 'dif_sqrt_surface_area']
+# feats = ['n_obs', 'n_obs_sp', 'n_obs_su', 'n_obs_au', 'obs_temp_mean',
+#        'obs_temp_std', 'obs_temp_skew', 'obs_temp_kurt', 'ad_zero_temp_doy',
+#        'obs_temp_mean_airdif', 'dif_SDF', 'dif_k_d', 'dif_surface_area',
+#        'dif_sw_mean', 'dif_lw_std', 'dif_at_std', 'dif_rh_mean', 'dif_rh_std',
+#        'dif_rain_mean', 'dif_rain_std', 'dif_sw_std_sp', 'dif_at_mean_sp',
+#        'dif_sw_mean_su', 'dif_sw_std_su', 'dif_lw_std_su', 'dif_rh_mean_su',
+#        'dif_rh_std_su', 'dif_ws_mean_su', 'dif_rain_mean_su',
+#        'dif_snow_mean_su', 'dif_snow_std_su', 'dif_sw_mean_au',
+#        'dif_lw_std_au', 'dif_at_mean_au', 'dif_at_std_au', 'dif_rh_mean_au',
+#        'dif_rh_std_au', 'dif_ws_mean_au', 'dif_rain_mean_au',
+#        'dif_rain_std_au', 'dif_snow_mean_au', 'dif_sw_std_wi',
+#        'dif_rh_mean_wi', 'dif_ws_mean_wi', 'dif_zero_temp_doy',
+#        'dif_ws_sp_mix', 'perc_dif_surface_area', 'dif_sqrt_surface_area']
 
 ###################################################################################
 
@@ -100,9 +100,9 @@ for _, lake_id in enumerate(train_lakes):
     new_df = pd.DataFrame()
 
     #get performance results (metatargets), filter out target as source
-    # lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results",header=None,names=['source_id','rmse'])
+    lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results",header=None,names=['source_id','rmse'])
     # lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results_noTran_noPre",header=None,names=['source_id','rmse'])
-    lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results_noTran_wPre",header=None,names=['source_id','rmse'])
+    # lake_df_res = pd.read_csv("../../results/transfer_learning/target_"+lake_id+"/PGDL_transfer_results_noTran_wPre",header=None,names=['source_id','rmse'])
     lake_df_res = lake_df_res[lake_df_res.source_id != 'source_id']
 
     #get metadata differences between target and all the sources
