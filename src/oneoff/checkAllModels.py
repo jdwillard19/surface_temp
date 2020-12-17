01f787c0c83b7da634297bfb8d26b3d1ea82adff
Jared W <willa099@umn.edu>
2020-12-17 13:05:46 -0600

    move to remote

--- src/oneoff/checkAllModels.py
+++ src/oneoff/checkAllModels.py
@@ -24,10 +24,10 @@ for lake_id in train_lakes:
     # lake_df = pd.read_feather("../../metadata/diffs/target_nhdhr_"+lake_id+".feather")
     # lake_df = lake_df[np.isin(lake_df['site_id'], train_lakes_wp)]
     # lake_df2 = lake_df[:]
-    lake_df_res = lake_df_res[np.isin(lake_df_res['source_id'], train_lakes)]
-    lake_df_res2 = lake_df_res2[np.isin(lake_df_res2['source_id'], train_lakes)]
-    lake_df_res['source_id2'] = ['nhdhr_'+str(x) for x in lake_df_res['source_id'].values]
-    lake_df_res2['source_id2'] = ['nhdhr_'+str(x) for x in lake_df_res2['source_id'].values]
+    # lake_df_res = lake_df_res[np.isin(lake_df_res['source_id'], train_lakes)]
+    # lake_df_res2 = lake_df_res2[np.isin(lake_df_res2['source_id'], train_lakes)]
+    # lake_df_res['source_id2'] = ['nhdhr_'+str(x) for x in lake_df_res['source_id'].values]
+    # lake_df_res2['source_id2'] = ['nhdhr_'+str(x) for x in lake_df_res2['source_id'].values]
     # lake_df = pd.merge(left=lake_df, right=lake_df_res.astype('object'), left_on='site_id', right_on='source_id2')
     # lake_df2 = pd.merge(left=lake_df2, right=lake_df_res2.astype('object'), left_on='site_id', right_on='source_id2')
     # new_df = lake_df
