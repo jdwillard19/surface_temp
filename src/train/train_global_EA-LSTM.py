cc702e3b2f3497b041441080e53508114e785c6c
Jared W <willa099@umn.edu>
2020-12-22 16:42:19 -0600

    move to remote

--- src/train/train_global_EA-LSTM.py
+++ src/train/train_global_EA-LSTM.py
@@ -602,7 +602,7 @@ for epoch in range(n_eps):
 
                 #run model
                 h_state = None
-                lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
+                # lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
                 pred, h_state = lstm_net(inputs, h_state)
                 pred = pred.view(pred.size()[0],-1)
                 pred = pred[:, begin_loss_ind:]
