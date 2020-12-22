5005d536be6d993b5a1ae61e6f3302c0991002e4
Jared W <willa099@umn.edu>
2020-12-22 16:23:30 -0600

    move to remote

--- src/train/train_global_EA-LSTM.py
+++ src/train/train_global_EA-LSTM.py
@@ -527,7 +527,7 @@ for epoch in range(n_eps):
         # lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
         # lstm_net.reset_parameters()
         # h_state = None
-        outputs, h_state, c_state = lstm_net(inputs[:,:,:n_features], inputs[0,:,n_features:])
+        outputs, h_state, c_state = lstm_net(inputs[:,:,:n_features], inputs[:,0,n_features:])
         outputs = outputs.view(outputs.size()[0],-1)
 
         #calculate losses
