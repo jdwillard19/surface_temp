d4af1d32e7d0b2f2da64ee42241efb12f3b99e4f
Jared W <willa099@umn.edu>
2020-12-22 16:08:08 -0600

    move to remote

--- src/train/train_global_EA-LSTM.py
+++ src/train/train_global_EA-LSTM.py
@@ -522,7 +522,7 @@ for epoch in range(n_eps):
             targets = targets.cuda()
 
         #forward  prop
-        lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
+        # lstm_net.hidden = lstm_net.init_hidden(batch_size=inputs.size()[0])
         lstm_net.reset_parameters()
         # h_state = None
         outputs, h_state, c_state = lstm_net(inputs[:n_features,:], inputs[n_features:,:])
