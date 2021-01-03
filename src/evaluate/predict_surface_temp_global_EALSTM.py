8c59a25a32e992e5924ca5b218f259d5c37f1f5b
Jared W <willa099@umn.edu>
2021-01-03 17:02:33 -0600

    move to remote

--- src/evaluate/predict_surface_temp_global_EALSTM.py
+++ src/evaluate/predict_surface_temp_global_EALSTM.py
@@ -41,7 +41,7 @@ k = 1
 output_to_file = True
 
 # save_file_path = "../../results/pgmtl_results_simpleGlobalLSTM.csv"
-save_file_path = "../../results/pgmtl_results_staticFeatGlobalLSTM.csv"
+save_file_path = "../../results/pgmtl_results_GlobalEALSTM.csv"
 
 #########################################################################################
 #paste features found in "pbmtl_feature_selection.py" here
@@ -145,16 +145,18 @@ for targ_ct, target_id in enumerate(test_lakes): #for each target lake
     n_test_dates_target = unique_tst_dates_target.shape[0]
 
 
-    #define LSTM model
+    #define LSTM model class
     class LSTM(nn.Module):
         def __init__(self, input_size, hidden_size, batch_size):
-            super(LSTM, self).__init__()
+            super(myLSTM_Net, self).__init__()
             self.input_size = input_size
             self.hidden_size = hidden_size
             self.batch_size = batch_size
-            self.lstm = nn.LSTM(input_size = n_total_features, hidden_size=hidden_size, batch_first=True,num_layers=num_layers) #batch_first=True?
-            self.out = nn.Linear(hidden_size, 1)
+            self.lstm = nn.LSTM(input_size = n_total_feats, hidden_size=hidden_size, batch_first=True,num_layers=num_layers,dropout=dropout) #batch_first=True?
+            self.out = nn.Linear(hidden_size, 1) #1?
             self.hidden = self.init_hidden()
+            self.w_upper_to_lower = []
+            self.w_lower_to_upper = []
 
         def init_hidden(self, batch_size=0):
             # initialize both hidden layers
@@ -167,8 +169,8 @@ for targ_ct, target_id in enumerate(test_lakes): #for each target lake
                 item1 = ret[1].cuda(non_blocking=True)
                 ret = (item0,item1)
             return ret
-        
-        def forward(self, x, hidden): #forward network propagation 
+
+        def forward(self, x, hidden):
             self.lstm.flatten_parameters()
             x = x.float()
             x, hidden = self.lstm(x, self.hidden)
@@ -176,16 +178,254 @@ for targ_ct, target_id in enumerate(test_lakes): #for each target lake
             x = self.out(x)
             return x, hidden
 
+    #method to calculate l1 norm of model
+    def calculate_l1_loss(model):
+        def l1_loss(x):
+            return torch.abs(x).sum()
 
+        to_regularize = []
+        # for name, p in model.named_parameters():
+        for name, p in model.named_parameters():
+            if 'bias' in name:
+                continue
+            else:
+                #take absolute value of weights and sum
+                to_regularize.append(p.view(-1))
+        l1_loss_val = torch.tensor(1, requires_grad=True, dtype=torch.float32)
+        l1_loss_val = l1_loss(torch.cat(to_regularize))
+        return l1_loss_val
+
+
+    class EALSTM(nn.Module):
+        """Implementation of the Entity-Aware-LSTM (EA-LSTM)
+        TODO: Include paper ref and latex equations
+        Parameters
+        ----------
+        input_size_dyn : int
+            Number of dynamic features, which are those, passed to the LSTM at each time step.
+        input_size_stat : int
+            Number of static features, which are those that are used to modulate the input gate.
+        hidden_size : int
+            Number of hidden/memory cells.
+        batch_first : bool, optional
+            If True, expects the batch inputs to be of shape [batch, seq, features] otherwise, the
+            shape has to be [seq, batch, features], by default True.
+        initial_forget_bias : int, optional
+            Value of the initial forget gate bias, by default 0
+        """
+
+        def __init__(self,
+                     input_size_dyn: int,
+                     input_size_stat: int,
+                     hidden_size: int,
+                     batch_first: bool = True,
+                     initial_forget_bias: int = 0):
+            super(EALSTM, self).__init__()
+
+            self.input_size_dyn = input_size_dyn
+            self.input_size_stat = input_size_stat
+            self.hidden_size = hidden_size
+            self.batch_first = batch_first
+            self.initial_forget_bias = initial_forget_bias
+
+            # create tensors of learnable parameters
+            self.weight_ih = nn.Parameter(torch.FloatTensor(input_size_dyn, 3 * hidden_size))
+            self.weight_hh = nn.Parameter(torch.FloatTensor(hidden_size, 3 * hidden_size))
+            self.weight_sh = nn.Parameter(torch.FloatTensor(input_size_stat, hidden_size))
+            self.bias = nn.Parameter(torch.FloatTensor(3 * hidden_size))
+            self.bias_s = nn.Parameter(torch.FloatTensor(hidden_size))
+
+            # initialize parameters
+            self.reset_parameters()
+
+        def reset_parameters(self):
+            """Initialize all learnable parameters of the LSTM"""
+            nn.init.orthogonal_(self.weight_ih.data)
+            nn.init.orthogonal_(self.weight_sh)
+
+            weight_hh_data = torch.eye(self.hidden_size)
+            weight_hh_data = weight_hh_data.repeat(1, 3)
+            self.weight_hh.data = weight_hh_data
+
+            nn.init.constant_(self.bias.data, val=0)
+            nn.init.constant_(self.bias_s.data, val=0)
+
+            if self.initial_forget_bias != 0:
+                self.bias.data[:self.hidden_size] = self.initial_forget_bias
+
+        def forward(self, x_d, x_s):
+            """[summary]
+            Parameters
+            ----------
+            x_d : torch.Tensor
+                Tensor, containing a batch of sequences of the dynamic features. Shape has to match
+                the format specified with batch_first.
+            x_s : torch.Tensor
+                Tensor, containing a batch of static features.
+            Returns
+            -------
+            h_n : torch.Tensor
+                The hidden states of each time step of each sample in the batch.
+            c_n : torch.Tensor]
+                The cell states of each time step of each sample in the batch.
+            """
+            if self.batch_first:
+                x_d = x_d.transpose(0, 1)
+                # x_s = x_s.transpose(0, 1)
+
+            seq_len, batch_size, _ = x_d.size()
+
+            h_0 = x_d.data.new(batch_size, self.hidden_size).zero_()
+            c_0 = x_d.data.new(batch_size, self.hidden_size).zero_()
+            h_x = (h_0, c_0)
+
+            # empty lists to temporally store all intermediate hidden/cell states
+            h_n, c_n = [], []
+            # expand bias vectors to batch size
+            bias_batch = (self.bias.unsqueeze(0).expand(batch_size, *self.bias.size()))
+
+            # calculate input gate only once because inputs are static
+            bias_s_batch = (self.bias_s.unsqueeze(0).expand(batch_size, *self.bias_s.size()))
+
+            i = torch.sigmoid(torch.addmm(bias_s_batch, x_s, self.weight_sh))
+
+            # perform forward steps over input sequence
+            for t in range(seq_len):
+                h_0, c_0 = h_x
+
+                # calculate gates
+                gates = (torch.addmm(bias_batch, h_0, self.weight_hh) +
+                         torch.mm(x_d[t], self.weight_ih))
+                f, o, g = gates.chunk(3, 1)
+
+                c_1 = torch.sigmoid(f) * c_0 + i * torch.tanh(g)
+                h_1 = torch.sigmoid(o) * torch.tanh(c_1)
+
+                # store intermediate hidden/cell state in list
+                h_n.append(h_1)
+                c_n.append(c_1)
+
+                h_x = (h_1, c_1)
+
+            h_n = torch.stack(h_n, 0)
+            c_n = torch.stack(c_n, 0)
+
+            if self.batch_first:
+                h_n = h_n.transpose(0, 1)
+                c_n = c_n.transpose(0, 1)
+
+            return h_n, c_n
+
+    class Model(nn.Module):
+        """Wrapper class that connects LSTM/EA-LSTM with fully connceted layer"""
+
+        def __init__(self,
+                     input_size_dyn: int,
+                     input_size_stat: int,
+                     hidden_size: int,
+                     initial_forget_bias: int = 5,
+                     dropout: float = 0.0,
+                     concat_static: bool = False,
+                     no_static: bool = False):
+            """Initialize model.
+            Parameters
+            ----------
+            input_size_dyn: int
+                Number of dynamic input features.
+            input_size_stat: int
+                Number of static input features (used in the EA-LSTM input gate).
+            hidden_size: int
+                Number of LSTM cells/hidden units.
+            initial_forget_bias: int
+                Value of the initial forget gate bias. (default: 5)
+            dropout: float
+                Dropout probability in range(0,1). (default: 0.0)
+            concat_static: bool
+                If True, uses standard LSTM otherwise uses EA-LSTM
+            no_static: bool
+                If True, runs standard LSTM
+            """
+            super(Model, self).__init__()
+            self.input_size_dyn = input_size_dyn
+            self.input_size_stat = input_size_stat
+            self.hidden_size = hidden_size
+            self.initial_forget_bias = initial_forget_bias
+            self.dropout_rate = dropout
+            self.concat_static = concat_static
+            self.no_static = no_static
+
+            if self.concat_static or self.no_static:
+                self.lstm = LSTM(input_size=input_size_dyn,
+                                 hidden_size=hidden_size,
+                                 initial_forget_bias=initial_forget_bias)
+            else:
+                self.lstm = EALSTM(input_size_dyn=input_size_dyn,
+                                   input_size_stat=input_size_stat,
+                                   hidden_size=hidden_size,
+                                   initial_forget_bias=initial_forget_bias)
+
+            self.dropout = nn.Dropout(p=dropout)
+            self.fc = nn.Linear(hidden_size, 1)
+
+        def forward(self, x_d: torch.Tensor, x_s: torch.Tensor = None) \
+                -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+            """Run forward pass through the model.
+            Parameters
+            ----------
+            x_d : torch.Tensor
+                Tensor containing the dynamic input features of shape [batch, seq_length, n_features]
+            x_s : torch.Tensor, optional
+                Tensor containing the static catchment characteristics, by default None
+            Returns
+            -------
+            out : torch.Tensor
+                Tensor containing the network predictions
+            h_n : torch.Tensor
+                Tensor containing the hidden states of each time step
+            c_n : torch,Tensor
+                Tensor containing the cell states of each time step
+            """
+            if self.concat_static or self.no_static:
+                h_n, c_n = self.lstm(x_d)
+            else:
+                h_n, c_n = self.lstm(x_d, x_s)
+            h_n = self.dropout(h_n)
+            # last_h = self.dropout(h_n[:, -1, :])
+            out = self.fc(h_n)
+            return out, h_n, c_n
+
+        # © 2020 GitHub, Inc.
+        # Terms
+        # Privacy
+        # Security
+        # Status
+        # Help
+
+        # Contact GitHub
+        # Pricing
+        # API
+        # Training
+        # Blog
+        # About
+
+
+
+
+    # lstm_net = myLSTM_Net(n_total_feats, n_hidden, batch_size)
+    lstm_net = Model(input_size_dyn=7,input_size_stat=13,hidden_size=n_hidden)
+    #tell model to use GPU if needed
+    if use_gpu:
+        lstm_net = lstm_net.cuda()
 
 
     #load source model
     # load_path = "../../models/global_model_16hid_2layer_final"
-    load_path = "../../models/global_model_16hid_1layer_final_wStatic"
+    # load_path = "../../models/global_model_16hid_1layer_final_wStatic"
     load_path = "../../models/global_model_128hid_1layer_final_wStaticEA"
     
     n_hidden = torch.load(load_path)['state_dict']['out.weight'].shape[1]
-    lstm_net = LSTM(n_total_features, n_hidden, batch_size)
+    lstm_net = Model(input_size_dyn=7,input_size_stat=13,hidden_size=n_hidden)
+    # lstm_net = LSTM(n_total_features, n_hidden, batch_size)
     if use_gpu:
         lstm_net = lstm_net.cuda(0)
     pretrain_dict = torch.load(load_path)['state_dict']
