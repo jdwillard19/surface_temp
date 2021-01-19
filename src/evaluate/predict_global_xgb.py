ed8d64f62a6bccaf14f3ad1bcbeb8be54e8b917d
Jared W <willa099@umn.edu>
2021-01-19 13:05:50 -0600

    move to remote

--- src/evaluate/predict_global_xgb.py
+++ src/evaluate/predict_global_xgb.py
@@ -63,3 +63,6 @@ for site_ct, site_id in enumerate(test_lakes):
     print("rmse: ", rmse)
     result_df.append(pd.DataFrame(['nhdhr_'+site_id, rmse]))
 
+pirnt("median rmse ", np.median(result_df['rmse']))
+pirnt("q1 rmse ", np.quantile(result_df['rmse'],.25))
+pirnt("q3 rmse ", np.quantile(result_df['rmse'],.75))
