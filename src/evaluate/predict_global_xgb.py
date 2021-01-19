4ded8abc004f94b5d9f70fea8f6f65cc960aaece
Jared W <willa099@umn.edu>
2021-01-19 14:48:16 -0600

    move to remote

--- src/evaluate/predict_global_xgb.py
+++ src/evaluate/predict_global_xgb.py
@@ -37,13 +37,12 @@ columns = ['ShortWave','LongWave','AirTemp','WindSpeed','Surface_Area','Surface_
 feat_inds = [0,1,2,4,8]
 train_df = pd.DataFrame(columns=columns)
 
-param_search = False
-
+lookback = 4
 
 #build training set
 for site_ct, site_id in enumerate(test_lakes):
-    if site_ct < 58:
-        continue
+    # if site_ct < 58:
+    #     continue
     print("predicting site ",site_ct,"/",len(test_lakes))
     #load data
     feats = np.load("../../data/processed/"+site_id+"/features_ea.npy")
@@ -51,15 +50,15 @@ for site_ct, site_id in enumerate(test_lakes):
     # dates = np.load("../../data/processed/"+name+"/dates.npy")
     data = np.concatenate((feats[:,feat_inds],labs.reshape(labs.shape[0],1)),axis=1)
 
-    #remove days without obs
-    data = data[np.where(np.isfinite(data[:,-1]))]
     X = data[:,:-1]
     y = data[:,-1]
-    lookback = 3
     if lookback > 0:
         X = np.array([np.append(X[i,:],X[i-lookback:i,:4].flatten()) for i in np.arange(lookback,X.shape[0])],dtype = np.half)
         y = y[lookback:]
-    pdb.set_trace()
+    #remove days without obs
+    data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
+    #remove days without obs
+    data = data[np.where(np.isfinite(data[:,-1]))]
     y_pred = model.predict(X)
     y_act = y
     rmse = calc_rmse(y_pred,y_act)
