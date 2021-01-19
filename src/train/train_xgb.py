6be9703dc80eaa9b036c7a9218163cc1a8e2534b
Jared W <willa099@umn.edu>
2021-01-19 13:24:57 -0600

    move to remote

--- src/train/train_xgb.py
+++ src/train/train_xgb.py
@@ -52,7 +52,7 @@ for ct, lake_id in enumerate(train_lakes):
         X = np.array([np.append(X[i,:],X[i-lookback:i,:4].flatten()) for i in np.arange(lookback,X.shape[0])],dtype = np.half)
         y = y[lookback:]
     #remove days without obs
-    data = np.concatenate((X,y),axis=1)
+    data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
 
     data = data[np.where(np.isfinite(data[:,-1]))]
     new_df = pd.DataFrame(columns=columns,data=data)
