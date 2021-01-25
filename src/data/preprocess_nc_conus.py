d238a9c6dc4689108c0de83bc2eb98d098463d68
Jared W <willa099@umn.edu>
2021-01-25 17:42:34 -0600

    move to remote

--- src/data/preprocess_nc_conus.py
+++ src/data/preprocess_nc_conus.py
@@ -32,9 +32,15 @@ sw_ds_path = "../../data/globus/NLDAS_DSWRFsfc_19790102-20210102_train_test.nc" 
 # lw_ds = xr.open_dataset(lw_ds_path)
 # at_ds = xr.open_dataset(at_ds_path)
 # sw_ds = xr.open_dataset(sw_ds_path)
+print("loading sw nc file....")
 sw_da = xr.open_dataset(sw_ds_path)['DSWRFsfc']
+print("sw file loaded")
+print("loading lw nc file....")
 lw_da = xr.open_dataset(lw_ds_path)['DLWRFsfc']
+print("lw file loaded")
+print("loading at nc file....")
 at_da = xr.open_dataset(at_ds_path)['TMP2m']
+print("at file loaded")
 
 dates = sw_da['Time'].values
 n_dyn_feats = 3 #AT,LW,SW
