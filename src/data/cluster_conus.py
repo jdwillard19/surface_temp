e50d7aa8e83eb1aea2098f085afca9b233260115
Jared W <willa099@umn.edu>
2021-01-26 15:32:41 -0600

    move to remote

--- src/data/cluster_conus.py
+++ src/data/cluster_conus.py
@@ -3,8 +3,32 @@ import numpy as np
 import pdb
 import sys
 import os
+import plotly.express as px
+from sklearn.cluster import KMeans
 
 metadata = pd.read_csv("../../metadata/surface_lake_metadata_conus.csv")
 obs = pd.read_feather("../../data/raw/obs/temp_wqp_munged.feather")
 
+site_ids = np.unique(obs['site_id'].values)
+metadata = metadata[np.isin(metadata['site_id'],site_ids)]
+metadata['log_area'] = np.log(metadata['area_m2'].values)
+
+normalize = True
+if normalize:
+	metadata['lat'] = (metadata['lat'].values - metadata['lat'].values.mean()) / metadata['lat'].values.std()
+	metadata['lon'] = (metadata['lon'].values - metadata['lon'].values.mean()) / metadata['lon'].values.std()
+	metadata['log_area'] = (metadata['log_area'].values - metadata['log_area'].values.mean()) / metadata['log_area'].values.std()
+print("starting clustering...")
+n_clusters = 16
+kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(metadata[['lat','lon','log_area']].values)
+print("clustering done!")
+metadata['cluster'] = kmeans.labels_
+# fig = px.scatter_3d(metadata, x='lon', y='lat', z='log_area',color='cluster')
+fig = px.scatter(metadata, x='lon', y='lat',color='cluster')
+                    #color='petal_length', symbol='species')
+
+fig.update_traces(marker=dict(size=2),
+                  selector=dict(mode='markers'))
+fig.write_html("clustered_lakes_2d_conus_"+str(n_clusters)+".html")
+fig.show()
 
--- src/data/write_NLDAS_xy_pairs.py
+++ src/data/write_NLDAS_xy_pairs.py
@@ -24,23 +24,28 @@ site_ids = np.unique(obs['site_id'].values)
 n_lakes = site_ids.shape[0]
 
 #load NLDAS data
-lw_ds_path = "../../data/globus/NLDAS_DLWRFsfc_19790102-20210102_train_test.nc" #longwave
-at_ds_path = "../../data/globus/NLDAS_TMP2m_19790102-20210102_train_test.nc" #airtemp
-sw_ds_path = "../../data/globus/NLDAS_DSWRFsfc_19790102-20210102_train_test.nc" #shortwave
-#wsu_ds_path=
+# lw_ds_path = "../../data/globus/NLDAS_DLWRFsfc_19790102-20210102_train_test.nc" #longwave
+# at_ds_path = "../../data/globus/NLDAS_TMP2m_19790102-20210102_train_test.nc" #airtemp
+# sw_ds_path = "../../data/globus/NLDAS_DSWRFsfc_19790102-20210102_train_test.nc" #shortwave
+wsu_ds_path = "../../data/globus/NLDAS_UGRD10m_19790102-20210102_train_test.nc" #windspeed u
+wsv_ds_path = "../../data/globus/NLDAS_VGRD10m_19790102-20210102_train_test.nc" #windspeed v
 
-# lw_ds = xr.open_dataset(lw_ds_path)
-# at_ds = xr.open_dataset(at_ds_path)
-# sw_ds = xr.open_dataset(sw_ds_path)
-print("loading sw nc file....")
-sw_da = xr.open_dataset(sw_ds_path)['DSWRFsfc']
-print("sw file loaded")
-print("loading lw nc file....")
-lw_da = xr.open_dataset(lw_ds_path)['DLWRFsfc']
-print("lw file loaded")
+# print("loading sw nc file....")
+# sw_da = xr.open_dataset(sw_ds_path)['DSWRFsfc']
+# print("sw file loaded")
+# print("loading lw nc file....")
+# lw_da = xr.open_dataset(lw_ds_path)['DLWRFsfc']
+# print("lw file loaded")
+# print("loading at nc file....")
+# at_da = xr.open_dataset(at_ds_path)['TMP2m']
+# print("at file loaded")
+print("loading at wsu file....")
+wsu_da = xr.open_dataset(wsu_ds_path)['UGRD10m']
+print("wsu file loaded")
 print("loading at nc file....")
-at_da = xr.open_dataset(at_ds_path)['TMP2m']
-print("at file loaded")
+wsv_da = xr.open_dataset(wsv_ds_path)['VGRD10m']
+print("wsv file loaded")
+
 
 
 for lake_ind, name in enumerate(site_ids):
@@ -51,10 +56,14 @@ for lake_ind, name in enumerate(site_ids):
     x = metadata[metadata['site_id'] == name]['x'].values[0]
     y = metadata[metadata['site_id'] == name]['y'].values[0]
 
-    sw_vals = sw_da[:,y,x].values
-    lw_vals = lw_da[:,y,x].values
-    at_vals = sw_da[:,y,x].values
-    np.save("../../data/raw/feats/SW_"+str(x)+"x_"+str(y)+"y",sw_vals)
-    np.save("../../data/raw/feats/LW_"+str(x)+"x_"+str(y)+"y",lw_vals)
-    np.save("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y",at_vals)
+    # sw_vals = sw_da[:,y,x].values
+    # lw_vals = lw_da[:,y,x].values
+    # at_vals = sw_da[:,y,x].values
+    wsu_vals = wsu_da[:,y,x].values
+    wsv_vals = vsu_da[:,y,x].values
+    # np.save("../../data/raw/feats/SW_"+str(x)+"x_"+str(y)+"y",sw_vals)
+    # np.save("../../data/raw/feats/LW_"+str(x)+"x_"+str(y)+"y",lw_vals)
+    # np.save("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y",at_vals)
+    np.save("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y",wsu_vals)
+    np.save("../../data/raw/feats/WSV_"+str(x)+"x_"+str(y)+"y",wsv_vals)
 
