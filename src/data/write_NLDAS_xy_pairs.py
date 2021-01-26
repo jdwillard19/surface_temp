3add5f61f8e11dadc1bd5af2d8989781d2fc19d0
Jared W <willa099@umn.edu>
2021-01-26 17:03:14 -0600

    move to remote

--- src/data/write_NLDAS_xy_pairs.py
+++ src/data/write_NLDAS_xy_pairs.py
@@ -25,10 +25,10 @@ n_lakes = site_ids.shape[0]
 
 #load NLDAS data
 # lw_ds_path = "../../data/globus/NLDAS_DLWRFsfc_19790102-20210102_train_test.nc" #longwave
-# at_ds_path = "../../data/globus/NLDAS_TMP2m_19790102-20210102_train_test.nc" #airtemp
+at_ds_path = "../../data/globus/NLDAS_TMP2m_19790102-20210102_train_test.nc" #airtemp
 # sw_ds_path = "../../data/globus/NLDAS_DSWRFsfc_19790102-20210102_train_test.nc" #shortwave
-wsu_ds_path = "../../data/globus/NLDAS_UGRD10m_19790102-20210102_train_test.nc" #windspeed u
-wsv_ds_path = "../../data/globus/NLDAS_VGRD10m_19790102-20210102_train_test.nc" #windspeed v
+# wsu_ds_path = "../../data/globus/NLDAS_UGRD10m_19790102-20210102_train_test.nc" #windspeed u
+# wsv_ds_path = "../../data/globus/NLDAS_VGRD10m_19790102-20210102_train_test.nc" #windspeed v
 
 # print("loading sw nc file....")
 # sw_da = xr.open_dataset(sw_ds_path)['DSWRFsfc']
@@ -36,15 +36,15 @@ wsv_ds_path = "../../data/globus/NLDAS_VGRD10m_19790102-20210102_train_test.nc" 
 # print("loading lw nc file....")
 # lw_da = xr.open_dataset(lw_ds_path)['DLWRFsfc']
 # print("lw file loaded")
-# print("loading at nc file....")
-# at_da = xr.open_dataset(at_ds_path)['TMP2m']
-# print("at file loaded")
-print("loading at wsu file....")
-wsu_da = xr.open_dataset(wsu_ds_path)['UGRD10m']
-print("wsu file loaded")
 print("loading at nc file....")
-wsv_da = xr.open_dataset(wsv_ds_path)['VGRD10m']
-print("wsv file loaded")
+at_da = xr.open_dataset(at_ds_path)['TMP2m']
+print("at file loaded")
+# print("loading at wsu file....")
+# wsu_da = xr.open_dataset(wsu_ds_path)['UGRD10m']
+# print("wsu file loaded")
+# print("loading at nc file....")
+# wsv_da = xr.open_dataset(wsv_ds_path)['VGRD10m']
+# print("wsv file loaded")
 
 
 # site_ids = np.flipud(site_ids)
@@ -56,16 +56,16 @@ for lake_ind, name in enumerate(site_ids):
     #get NLDAS coords
     x = metadata[metadata['site_id'] == name]['x'].values[0]
     y = metadata[metadata['site_id'] == name]['y'].values[0]
-    if os.path.exists("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y"):
+    if os.path.exists("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y"):
         continue
     # sw_vals = sw_da[:,y,x].values
     # lw_vals = lw_da[:,y,x].values
-    # at_vals = sw_da[:,y,x].values
-    wsu_vals = wsu_da[:,y,x].values
-    wsv_vals = wsv_da[:,y,x].values
+    at_vals = at_da[:,y,x].values
+    # wsu_vals = wsu_da[:,y,x].values
+    # wsv_vals = wsv_da[:,y,x].values
     # np.save("../../data/raw/feats/SW_"+str(x)+"x_"+str(y)+"y",sw_vals)
     # np.save("../../data/raw/feats/LW_"+str(x)+"x_"+str(y)+"y",lw_vals)
-    # np.save("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y",at_vals)
-    np.save("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y",wsu_vals)
-    np.save("../../data/raw/feats/WSV_"+str(x)+"x_"+str(y)+"y",wsv_vals)
+    np.save("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y",at_vals)
+    # np.save("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y",wsu_vals)
+    # np.save("../../data/raw/feats/WSV_"+str(x)+"x_"+str(y)+"y",wsv_vals)
 
