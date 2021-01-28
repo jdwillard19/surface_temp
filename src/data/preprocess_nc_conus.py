efabf7816873f0c4f8d5d62967e9d73bf7a09d11
Jared W <willa099@umn.edu>
2021-01-27 18:14:43 -0600

    move to remote

--- src/data/preprocess_nc_conus.py
+++ src/data/preprocess_nc_conus.py
@@ -53,46 +53,46 @@ if not hardcode:
         print("(",lake_ind,"/",str(len(site_ids)),") ","pre ", name)
 
         #get NLDAS coords
-        # x = int(metadata.loc[name]['x'])-1
-        # y = int(metadata.loc[name]['y'])-1
-        # sw_vals = np.load("../../data/raw/feats/SW_"+str(x)+"x_"+str(y)+"y.npy")
-        # lw_vals = np.load("../../data/raw/feats/LW_"+str(x)+"x_"+str(y)+"y.npy")
-        # at_vals = np.load("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y.npy")
-        # wsu_vals = np.load("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y.npy")
-        # wsv_vals = np.load("../../data/raw/feats/WSV_"+str(x)+"x_"+str(y)+"y.npy")
-        # means_per_lake[lake_ind,0] = sw_vals.mean()
-        # means_per_lake[lake_ind,1] = lw_vals.mean()
-        # means_per_lake[lake_ind,2] = at_vals.mean()
-        # means_per_lake[lake_ind,3] = wsu_vals.mean()
-        # means_per_lake[lake_ind,4] = wsv_vals.mean()
-        # var_per_lake[lake_ind,0] = sw_vals.std()
-        # var_per_lake[lake_ind,1] = lw_vals.std()
-        # var_per_lake[lake_ind,2] = at_vals.std()
-        # var_per_lake[lake_ind,3] = wsu_vals.std()
-        # var_per_lake[lake_ind,4] = wsv_vals.std()
-        # if not np.isfinite(means_per_lake[lake_ind,:]).all():
-        # 	pdb.set_trace()
-        # 	assert np.isfinite(means_per_lake[lake_ind,:]).all()
-        # if not np.isfinite(var_per_lake[lake_ind,:]).all():
-        # 	pdb.set_trace()
-        # 	assert np.isfinite(var_per_lake[lake_ind,:]).all()
-        stat_vals_per_lake[lake_ind,0] = metadata.loc[name].area_m2
-        stat_vals_per_lake[lake_ind,1] = metadata.loc[name].lat
-        stat_vals_per_lake[lake_ind,2] = metadata.loc[name].lon
+        x = int(metadata.loc[name]['x'])-1
+        y = int(metadata.loc[name]['y'])-1
+        sw_vals = np.load("../../data/raw/feats/SW_"+str(x)+"x_"+str(y)+"y.npy")
+        lw_vals = np.load("../../data/raw/feats/LW_"+str(x)+"x_"+str(y)+"y.npy")
+        at_vals = np.load("../../data/raw/feats/AT_"+str(x)+"x_"+str(y)+"y.npy")
+        wsu_vals = np.load("../../data/raw/feats/WSU_"+str(x)+"x_"+str(y)+"y.npy")
+        wsv_vals = np.load("../../data/raw/feats/WSV_"+str(x)+"x_"+str(y)+"y.npy")
+        means_per_lake[lake_ind,0] = sw_vals.mean()
+        means_per_lake[lake_ind,1] = lw_vals.mean()
+        means_per_lake[lake_ind,2] = at_vals.mean()
+        means_per_lake[lake_ind,3] = wsu_vals.mean()
+        means_per_lake[lake_ind,4] = wsv_vals.mean()
+        var_per_lake[lake_ind,0] = sw_vals.std()
+        var_per_lake[lake_ind,1] = lw_vals.std()
+        var_per_lake[lake_ind,2] = at_vals.std()
+        var_per_lake[lake_ind,3] = wsu_vals.std()
+        var_per_lake[lake_ind,4] = wsv_vals.std()
+        if not np.isfinite(means_per_lake[lake_ind,:]).all():
+        	pdb.set_trace()
+        	assert np.isfinite(means_per_lake[lake_ind,:]).all()
+        if not np.isfinite(var_per_lake[lake_ind,:]).all():
+        	pdb.set_trace()
+        	assert np.isfinite(var_per_lake[lake_ind,:]).all()
+        # stat_vals_per_lake[lake_ind,0] = metadata.loc[name].area_m2
+        # stat_vals_per_lake[lake_ind,1] = metadata.loc[name].lat
+        # stat_vals_per_lake[lake_ind,2] = metadata.loc[name].lon
 
 
-    # mean_feats = np.average(means_per_lake, axis=0)   
-    # std_feats = np.average(var_per_lake ** (.5), axis=0)   
-    # print("mean feats: ", repr(mean_feats))
-    # print("std feats: ", repr(std_feats))
+    mean_feats = np.average(means_per_lake, axis=0)   
+    std_feats = np.average(var_per_lake ** (.5), axis=0)   
+    print("mean feats: ", repr(mean_feats))
+    print("std feats: ", repr(std_feats))
     # assert mean_feats.shape[0] == 8
     # assert std_feats.shape[0] == 8
     # assert not np.isnan(np.sum(mean_feats))
     # assert not np.isnan(np.sum(std_feats))
 else:
 	#can uncomment and hard code here 
-	mean_feats = np.array([8669184.835544042, 4.09564144e+01, -9.01653002e+01,1.66308346e02, 2.91540662e02, 6.68199233e00, 7.37268070e01, 4.79260805e00, 1.81936454e-03, 2.30189504e-03])
-	std_feats = np.array([517506195.05362266, 6.51122458e+00, 1.04199758e+01, 8.52790273e+01, 6.10175316e+01, 1.28183124e+01, 1.29724391e+01, 1.69513213e+00, 5.54588726e-03, 1.27910016e-02])
+	# mean_feats = np.array([8669184.835544042, 4.09564144e+01, -9.01653002e+01,1.66308346e02, 2.91540662e02, 6.68199233e00, 7.37268070e01, 4.79260805e00, 1.81936454e-03, 2.30189504e-03])
+	# std_feats = np.array([517506195.05362266, 6.51122458e+00, 1.04199758e+01, 8.52790273e+01, 6.10175316e+01, 1.28183124e+01, 1.29724391e+01, 1.69513213e+00, 5.54588726e-03, 1.27910016e-02])
 
 pdb.set_trace()
 sw_ds_path = "../../data/globus/NLDAS_DSWRFsfc_19790102-20210102_train_test.nc" #shortwave
