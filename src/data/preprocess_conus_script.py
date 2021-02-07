8434abd8ca6bde451a99153db04f5c37df588522
Jared W <willa099@umn.edu>
2021-02-07 12:47:50 -0600

    move to remote

--- src/data/preprocess_conus_script.py
+++ src/data/preprocess_conus_script.py
@@ -15,7 +15,7 @@ import datetime
 
 #load metadata, get ids
 # metadata = pd.read_csv("../../metadata/surface_lake_metadata_conus.csv")
-metadata = pd.read_csv("../../metadata/surface_lake_metadata_file_temp.csv")
+metadata = pd.read_csv("../../metadata/surface_lake_metadata_file_020421.csv")
 site_ids = np.unique(metadata['site_id'].values)
 
 # metadata.set_index("site_id",inplace=True)
