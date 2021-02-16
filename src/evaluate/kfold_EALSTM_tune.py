0fcaaee47b9ebc1045d11254f7c2de3aa95b6317
Jared W <willa099@umn.edu>
2021-02-15 20:32:22 -0600

    move to remote

--- src/evaluate/kfold_EALSTM_tune.py
+++ src/evaluate/kfold_EALSTM_tune.py
@@ -199,7 +199,7 @@ for k in range(n_folds):
 
 
     #load val/test data into enumerator based on batch size
-    # testloader = torch.utils.data.DataLoader(tst_data, batch_size=tst_data.size()[0], shuffle=False, pin_memory=True)
+    testloader = torch.utils.data.DataLoader(tst_data, batch_size=tst_data.size()[0], shuffle=False, pin_memory=True)
 
 
     #define EA-LSTM class
