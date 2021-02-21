f0d0c8ec10c91d7c4fbe54d9e11c48da9f9d2c63
Jared W <willa099@umn.edu>
2021-02-21 13:46:03 -0600

    move to remote

---
+++ src/hpc/xgb_hypertune_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --time=23:59:00
+#SBATCH --ntasks=8
+#SBATCH --mem=20g
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=willa099@umn.edu
+#SBATCH --output=train_xgb_conus.out
+#SBATCH --error=train_xgb_conus.err
+#SBATCH --gres=gpu:k40:2
+#SBATCH -p k40
+source /home/kumarv/willa099/takeme_train.sh
+
+python train_xgb_conus.py
