c5c3e9c3dd72e1753e16993c332ecf1818398d70
Jared W <willa099@umn.edu>
2021-02-22 16:10:52 -0600

    move to remote

--- src/hpc/create_clusterEA_jobs.py
+++ src/hpc/create_clusterEA_jobs.py
@@ -36,8 +36,40 @@ for name in range(n_folds):
     with open('./jobs/job_{}_foldEA.sh'.format(l), 'w') as output:
         output.write(all)
 
+for name in range(n_folds):
+    ct += 1
+    #for each unique lake
+    print(name)
+    l = name
 
-compile_job_path= './jobs/sbatch_script_foldEA.sh'
+    # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=XGB_singlefold_%s.out\n#SBATCH --error=XGB_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+    script2 = "python xgb_error_est.py %s"%(l)
+    # script3 = "python singleModel_customSparse.py %s"%(l)
+    all= "\n".join([header,script,script2])
+    sbatch = "\n".join(["sbatch job_%s_foldXGB.sh"%(l),sbatch])
+    with open('./jobs/job_{}_foldXGB.sh'.format(l), 'w') as output:
+        output.write(all)
+
+for name in range(n_folds):
+    ct += 1
+    #for each unique lake
+    print(name)
+    l = name
+
+    # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=LM_singlefold_%s.out\n#SBATCH --error=LM_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+    script2 = "python linear_model_error_est.py %s"%(l)
+    # script3 = "python singleModel_customSparse.py %s"%(l)
+    all= "\n".join([header,script,script2])
+    sbatch = "\n".join(["sbatch job_%s_foldLM.sh"%(l),sbatch])
+    with open('./jobs/job_{}_foldLM.sh'.format(l), 'w') as output:
+        output.write(all)
+
+
+compile_job_path= './jobs/sbatch_script_error_est.sh'
 with open(compile_job_path, 'w') as output2:
     output2.write(sbatch)
 
