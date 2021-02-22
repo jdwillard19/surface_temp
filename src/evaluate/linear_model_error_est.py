3f9648c356f4d472f14f66398904c9b92061be76
Jared W <willa099@umn.edu>
2021-02-22 16:00:22 -0600

    move to remote

--- src/evaluate/linear_model_error_est.py
+++ src/evaluate/linear_model_error_est.py
@@ -132,6 +132,7 @@ for ct, lake_id in enumerate(test_lakes):
     result_df = result_df.append(df)
 
       # test_df = pd.concat([test_df, new_df], ignore_index=True)
+result_df.reset_index(inplace=True)
 result_df.to_feather("../../results/lm_conus_022221_fold"+str(k)+".feather")
 # if param_search:
 #     gbm = xgb.XGBRegressor(booster='gbtree',n_estimators=10000,learning_rate=.025,max_depth=6,min_child_weight=11,subsample=.8,colsample_bytree=.7)
