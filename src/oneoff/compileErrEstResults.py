8f8dd66e05be27c160953c21a64b817cb8a10e85
Jared W <willa099@umn.edu>
2021-02-24 15:39:41 -0600

    move to remote

--- src/oneoff/compileErrEstResults.py
+++ src/oneoff/compileErrEstResults.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import pdb
 
 metadata = pd.read_csv("../../metadata/surface_lake_metadata_021521_wCluster.csv")
 obs = pd.read_feather("../../data/raw/obs/surface_lake_temp_daily_020421.feather")
