95b6991cb2b35bc5d06f47fac3531863321762da
Jared W <willa099@umn.edu>
2021-02-24 15:52:22 -0600

    move to remote

--- src/evaluate/xgb_error_est_date_output.py
+++ src/evaluate/xgb_error_est_date_output.py
@@ -79,9 +79,9 @@ for ct, lake_id in enumerate(test_lakes):
         # y = y[inds]
         dates = dates[inds]
     #remove days without obs
-    data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
+    # data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
     # data = data[np.where(np.isfinite(data[:,-1]))]
-    dates = dates[np.where(np.isfinite(data[:,-1]))]
+    # dates = dates[np.where(np.isfinite(data[:,-1]))]
     # new_df = pd.DataFrame(columns=columns,data=data)
     # X = new_df[columns[:-1]].values
     # y_act = np.ravel(new_df[columns[-1]].values)
