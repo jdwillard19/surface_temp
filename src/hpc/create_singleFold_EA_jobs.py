fa3e6c7f257fba83f79bb2aa032795d6400e6c97
Jared W <willa099@umn.edu>
2021-02-26 15:22:46 -0600

    move to remote

--- src/hpc/create_singleFold_EA_jobs.py
+++ src/hpc/create_singleFold_EA_jobs.py
@@ -27,7 +27,7 @@ for name in range(n_folds):
     l = name
 
     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
-    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_singlefold_%s.out\n#SBATCH --error=EALSTM_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_singlefold_225hid_%s.out\n#SBATCH --error=EALSTM_singlefold_225hid_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
     script2 = "python EALSTM_error_estimation_and_output_single_fold.py %s"%(l)
     # script3 = "python singleModel_customSparse.py %s"%(l)
