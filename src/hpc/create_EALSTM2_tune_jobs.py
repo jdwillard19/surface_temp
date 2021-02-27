7601c0309e9191bf46c5f8d03ffdedfb26ca412a
Jared W <willa099@umn.edu>
2021-02-27 14:37:16 -0600

    move to remote

--- src/hpc/create_EALSTM2_tune_jobs.py
+++ src/hpc/create_EALSTM2_tune_jobs.py
@@ -17,7 +17,7 @@ for i in n_hid_arr:
     for k in range(n_folds):
     #for each unique lake
         # print("i'i,k)
-
+        ct += 1
         # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
         header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --gres=gpu:k40:2\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=ealstm_tune_2layer_%shid_%sfold.out\n#SBATCH --error=ealstm_tune_2layer_%shid_%sfold.err\n\n#SBATCH -p k40"%(i,k,i,k)
         script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
