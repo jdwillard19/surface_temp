c6167dd36d8016c68b7caa8982833a7901559319
Jared W <willa099@umn.edu>
2021-03-01 11:18:51 -0600

    move to remote

--- src/hpc/create_clusterEA_jobs.py
+++ src/hpc/create_clusterEA_jobs.py
@@ -20,6 +20,22 @@ n_folds = 5
 n_lakes = 0
 sbatch = ""
 ct = 0
+# for name in range(n_folds):
+#     ct += 1
+#     #for each unique lake
+#     print(name)
+#     l = name
+
+#     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
+#     header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_singlefold_%s.out\n#SBATCH --error=EALSTM_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+#     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+#     script2 = "python EALSTM_error_estimation_and_output_single_fold.py %s"%(l)
+#     # script3 = "python singleModel_customSparse.py %s"%(l)
+#     all= "\n".join([header,script,script2])
+#     sbatch = "\n".join(["sbatch job_%s_foldEA.sh"%(l),sbatch])
+#     with open('./jobs/job_{}_foldEA.sh'.format(l), 'w') as output:
+#         output.write(all)
+
 for name in range(n_folds):
     ct += 1
     #for each unique lake
@@ -27,23 +43,7 @@ for name in range(n_folds):
     l = name
 
     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
-    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EALSTM_singlefold_%s.out\n#SBATCH --error=EALSTM_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
-    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
-    script2 = "python EALSTM_error_estimation_and_output_single_fold.py %s"%(l)
-    # script3 = "python singleModel_customSparse.py %s"%(l)
-    all= "\n".join([header,script,script2])
-    sbatch = "\n".join(["sbatch job_%s_foldEA.sh"%(l),sbatch])
-    with open('./jobs/job_{}_foldEA.sh'.format(l), 'w') as output:
-        output.write(all)
-
-for name in range(n_folds):
-    ct += 1
-    #for each unique lake
-    print(name)
-    l = name
-
-    # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
-    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=XGB_singlefold_%s.out\n#SBATCH --error=XGB_singlefold_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
+    header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=XGB_singlefold_030121_%s.out\n#SBATCH --error=XGB_singlefold_030121_%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
     script2 = "python xgb_error_est.py %s"%(l)
     # script3 = "python singleModel_customSparse.py %s"%(l)
@@ -69,7 +69,7 @@ for name in range(n_folds):
 #         output.write(all)
 
 
-compile_job_path= './jobs/sbatch_script_error_est.sh'
+compile_job_path= './jobs/sbatch_script_error_est_xgb.sh'
 with open(compile_job_path, 'w') as output2:
     output2.write(sbatch)
 
