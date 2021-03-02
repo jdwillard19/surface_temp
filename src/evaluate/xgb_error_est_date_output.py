c564f83e9c74ba63649ac5cc1b7615d01924f9cb
Jared W <willa099@umn.edu>
2021-03-01 18:39:31 -0600

    move to remote

--- src/evaluate/xgb_error_est_date_output.py
+++ src/evaluate/xgb_error_est_date_output.py
@@ -72,10 +72,9 @@ for ct, lake_id in enumerate(test_lakes):
     y = data[:,-1]
     inds = np.where(np.isfinite(y))[0]
     # inds = inds[np.where(inds > farthest_lookback)[0]]
-    if lookback > 0:
         # X = np.array([np.append(np.append(np.append(X[i,:],X[i-lookback:i,4:].flatten()),X[i-14,4:]),X[i-30,4:]) for i in inds],dtype = np.float)
         # y = y[inds]
-        dates = dates[inds]
+    dates = dates[inds]
     #remove days without obs
     # data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
     # data = data[np.where(np.isfinite(data[:,-1]))]
