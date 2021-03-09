c00e690e5f855c86173d6efe20736a1b4f48c84b
Jared W <willa099@umn.edu>
2021-03-09 17:17:01 -0600

    move to remote

--- src/data/preprocess_conus_script_full40year.py
+++ src/data/preprocess_conus_script_full40year.py
@@ -73,7 +73,7 @@ site_ids = np.array(['nhdhr_139474232'])
 for site_ct, site_id in enumerate(site_ids):
     if site_id == 'nhdhr_{ef5a02dc-f608-4740-ab0e-de374bf6471c}' or site_id == 'nhdhr_136665792' or site_id == 'nhdhr_136686179':
         continue
-    if os.path.exists("../../data/processed/"+site_id+"/features_ea_conus_021621.npy"):
+    if os.path.exists("../../data/processed/"+site_id+"/features_ea_conus_021621.npy") and os.path.exists("../../data/processed/"+site_id+"/processed_features_ea_conus_021621.npy"):
         print("EXISTS")
         continue
     # if site_ct < 5383:
