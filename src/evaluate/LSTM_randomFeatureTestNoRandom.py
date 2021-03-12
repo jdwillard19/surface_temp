0a3441dd15934ace35b2d81677613c58f3f33382
Jared W <willa099@umn.edu>
2021-03-12 15:47:09 -0600

    move to remote

--- src/evaluate/LSTM_randomFeatureTestNoRandom.py
+++ src/evaluate/LSTM_randomFeatureTestNoRandom.py
@@ -158,6 +158,14 @@ if hypertune:
     val_data = trn_data[-4000:,:,:]
     trn_data = trn_data[:-4000,:,:]
 
+
+#add more random
+rand_to_add = 60
+rand_add_bool = True
+if rand_add_bool:
+    rand_data = np.random.random((trn_data.shape[0],trn_data.shape[1],60))
+    trn_data = torch.from_numpy(np.concatenate([rand_data,trn_data],axis=2))
+    n_static_feats = n_static_feats+rand_to_add
 # (_, _, tst_data, tst_dates, unique_tst_dates) = buildLakeDataForRNN_manylakes_gauged(lakenames, seq_length, n_features, \
 #                                             win_shift= win_shift, begin_loss_ind = 0, \
 #                                             outputFullTestMatrix=False, sparseCustom=None, \
@@ -797,6 +805,12 @@ for targ_ct, target_id in enumerate(lakenames): #for each target lake
                                                 outputFullTestMatrix=False, sparseCustom=None, \
                                                 allTestSeq=False, static_feats=True,n_static_feats=0,\
                                                 postProcessSplits=True)  
+    
+    if rand_add_bool:
+        rand_data = np.random.random((tst_data_target.shape[0],tst_data_target.shape[1],rand_to_add))
+        tst_data_target = torch.from_numpy(np.concatenate([rand_data,tst_data_target],axis=2))
+        n_static_feats = 64
+
     #useful values, LSTM params
     batch_size = tst_data_target.size()[0]
     n_test_dates_target = unique_tst_dates_target.shape[0]
