127ed82dacf0823ed7c09f2e5f7d44a62691611e
Jared W <willa099@umn.edu>
2021-03-12 14:54:11 -0600

    move to remote

--- src/oneoff/final_output_rmse_check.py
+++ src/oneoff/final_output_rmse_check.py
@@ -8,6 +8,7 @@ site_ids = metadata['site_id'].values
 
 sites = []
 rmses = []
+final_df = pd.DataFrame()
 def calc_rmse(predictions, targets):
     return np.sqrt(((predictions - targets) ** 2).mean()) 
 for site_ct,site_id in enumerate(site_ids):
@@ -19,8 +20,15 @@ for site_ct,site_id in enumerate(site_ids):
         df = pd.read_feather("../../results/SWT_results/outputs_"+site_id+".feather") 
         loss_outputs = df['temp_pred'].values[np.isfinite(df['temp_actual'].values)]
         loss_actual = df['temp_actual'].values[np.isfinite(df['temp_actual'].values)]
+        loss_dates = df['index'].values[np.isfinite(df['temp_actual'].values)]
+
+
+        df = pd.DataFrame(data={'date':loss_dates, 'pred':loss_outputs, 'actual':loss_actual})
+        final_df = pd.concat([final_df,df])
         pdb.set_trace()
-        sites.append(site_id)
-        rmse = calc_rmse(loss_outputs, loss_actual)
-        print("rmse: ",rmse)
-        rmses.append(rmse)
+
+        
+        # sites.append(site_id)
+        # rmse = calc_rmse(loss_outputs, loss_actual)
+        # print("rmse: ",rmse)
+        # rmses.append(rmse)
