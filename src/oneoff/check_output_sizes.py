87bed39a912494a40867965c337e0d8e547e2ddb
Jared W <willa099@umn.edu>
2021-03-15 08:58:50 -0500

    move to remote

--- src/oneoff/check_output_sizes.py
+++ src/oneoff/check_output_sizes.py
@@ -8,5 +8,5 @@ metadata = pd.read_csv("../../metadata/lake_metadata_full_conus_185k.csv")
 test_lakes = metadata['site_id'].values
 for i, target_id in enumerate(test_lakes):
 	print(i,"/",len(test_lakes))
-	df = pd.read_feather('outputs_'+target_id+'.feather')
+	df = pd.read_feather('../../results/SWT_results/outputs_'+target_id+'.feather')
 	assert df.shape[0] == 14976, "output not correct size"
