64052f5f7a4a4de8ad9c86ab1851ccc59917c689
Jared W <willa099@umn.edu>
2021-03-17 15:35:21 -0500

    move to remote

--- src/evaluate/kfold_EALSTM_tune.py
+++ src/evaluate/kfold_EALSTM_tune.py
@@ -75,7 +75,7 @@ num_layers = 1
 # lambda1 = 1e-
 lambda1 = 0
 og_k = int(sys.argv[2])
-folds_arr = np.delete(np.arange(5),[og_k])
+folds_arr = np.array(np.delete(np.arange(5),[og_k]))
 
 # n_eps = 10000
 n_eps = 10000
