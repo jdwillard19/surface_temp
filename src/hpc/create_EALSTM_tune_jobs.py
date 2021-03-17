15405bb198bc66a47dab71a0b6b9be21b8ac817f
Jared W <willa099@umn.edu>
2021-03-17 15:41:03 -0500

    move to remote

--- src/hpc/create_EALSTM_tune_jobs.py
+++ src/hpc/create_EALSTM_tune_jobs.py
@@ -11,15 +11,16 @@ import numpy as np
 
 sbatch = ""
 ct = 0
-n_hid_arr = np.arange(25,275,25)
-for i in n_hid_arr:
+# n_hid_arr = np.arange(25,275,25)
+k_arr = np.arange(5)
+for i in k_arr:
     #for each unique lake
     print(i)
 
     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
     header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --gres=gpu:k40:2\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=ealstm_tune_%s.out\n#SBATCH --error=ealstm_tune_%s.err\n\n#SBATCH -p k40"%(i,i)
     script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
-    script2 = "python kfold_EALSTM_tune2.py %s"%(i)
+    script2 = "python kfold_EALSTM_tune.py 128 %s"%(i)
     # script3 = "python singleModel_customSparse.py %s"%(l)
     all= "\n".join([header,script,script2])
     sbatch = "\n".join(["sbatch job_ealstm_tune_%s.sh"%(i),sbatch])
