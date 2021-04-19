f7b727f502b7da748a1aea9de6ae20a4c0555175
Jared W <willa099@umn.edu>
2021-04-19 10:59:50 -0500

    move to remote

--- src/oneoff/calc_err_per_fold.py
+++ src/oneoff/calc_err_per_fold.py
@@ -9,7 +9,7 @@ metadata = pd.read_csv("../../metadata/surface_lake_metadata_041421_wCluster.csv
 err_per_fold = np.empty((5))
 err_per_fold[:] = np.nan
 for k in range(5):
-	err_per_fold[k] = np.median(err_df[np.isin(err_df['site_id'],metadata[metadata['5fold_fold']==k]['rmse_ealstm'].values)
+	err_per_fold[k] = np.median(err_df[np.isin(err_df['site_id'],metadata[metadata['5fold_fold']==k])]['rmse_ealstm'].values)
 
 print(err_per_fold)
 
