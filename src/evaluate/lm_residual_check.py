5217270eae769edee522231e9955faeb5ab290cb
Jared W <willa099@umn.edu>
2021-06-19 18:55:16 -0500

    move to remote

--- src/evaluate/lm_residual_check.py
+++ src/evaluate/lm_residual_check.py
@@ -59,37 +59,40 @@ train_lakes = metadata['site_id'].values
 train_df = pd.DataFrame(columns=columns)
 # test_df = pd.DataFrame(columns=columns)
 
-for ct, lake_id in enumerate(train_lakes):
-    if ct %100 == 0:
-      print("fold ",k," assembling training lake ",ct,"/",len(train_lakes),": ",lake_id)
-    #load data
-    feats = np.load("../../data/processed/"+lake_id+"/features_ea_conus_021621.npy")
-    labs = np.load("../../data/processed/"+lake_id+"/full.npy")
-    # dates = np.load("../../data/processed/"+name+"/dates.npy")
-    data = np.concatenate((feats[:,:],labs.reshape(labs.shape[0],1)),axis=1)
-    X = data[:,:-1]
-    y = data[:,-1]
-    inds = np.where(np.isfinite(y))[0]
-    if inds.shape[0] == 0:
-        continue
-    # inds = inds[np.where(inds > farthest_lookback)]
+# for ct, lake_id in enumerate(train_lakes):
+#     if ct %100 == 0:
+#       print("fold ",k," assembling training lake ",ct,"/",len(train_lakes),": ",lake_id)
+#     #load data
+#     feats = np.load("../../data/processed/"+lake_id+"/features_ea_conus_021621.npy")
+#     labs = np.load("../../data/processed/"+lake_id+"/full.npy")
+#     # dates = np.load("../../data/processed/"+name+"/dates.npy")
+#     data = np.concatenate((feats[:,:],labs.reshape(labs.shape[0],1)),axis=1)
+#     X = data[:,:-1]
+#     y = data[:,-1]
+#     inds = np.where(np.isfinite(y))[0]
+#     if inds.shape[0] == 0:
+#         continue
+#     # inds = inds[np.where(inds > farthest_lookback)]
 
-    # if lookback > 0:
-        # X = np.array([np.append(np.append(np.append(X[i,:],X[i-lookback:i,4:].flatten()),X[i-14,4:]),X[i-30,4:]) for i in np.arange(farthest_lookback,X.shape[0])],dtype = np.half)
-    X = np.array([X[i,:] for i in inds],dtype = np.float)
-    # y = y[farthest_lookback:]
-    y = y[inds]
-    #remove days without obs
-    data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
+#     # if lookback > 0:
+#         # X = np.array([np.append(np.append(np.append(X[i,:],X[i-lookback:i,4:].flatten()),X[i-14,4:]),X[i-30,4:]) for i in np.arange(farthest_lookback,X.shape[0])],dtype = np.half)
+#     X = np.array([X[i,:] for i in inds],dtype = np.float)
+#     # y = y[farthest_lookback:]
+#     y = y[inds]
+#     #remove days without obs
+#     data = np.concatenate((X,y.reshape(len(y),1)),axis=1)
 
-    data = data[np.where(np.isfinite(data[:,-1]))]
-    new_df = pd.DataFrame(columns=columns,data=data)
-    train_df = pd.concat([train_df, new_df], ignore_index=True)
-X = train_df[columns[:-1]].values
-y = np.ravel(train_df[columns[-1]].values)
+#     data = data[np.where(np.isfinite(data[:,-1]))]
+#     new_df = pd.DataFrame(columns=columns,data=data)
+#     train_df = pd.concat([train_df, new_df], ignore_index=True)
+# X = train_df[columns[:-1]].values
+# y = np.ravel(train_df[columns[-1]].values)
 
-np.save("./lm_train_x",X)
-np.save("./lm_train_y",y)
+# np.save("./lm_train_x",X)
+# np.save("./lm_train_y",y)
+
+X = np.load("./lm_train_x.npy")
+y = np.load("./lm_train_y.npy")
 print("train set dimensions: ",X.shape)
 #construct lookback feature set??
 model = LinearRegression()
@@ -101,7 +104,7 @@ print("model trained and saved to ", save_file_path)
 
 #test
 for ct, lake_id in enumerate(train_lakes):
-    print("fold ",k," testing test lake ",ct,"/",len(test_lakes),": ",lake_id)
+    print("fold ",k," testing test lake ",ct,"/",len(train_lakes),": ",lake_id)
     #load data
     feats = np.load("../../data/processed/"+lake_id+"/features_ea_conus_021621.npy")
     labs = np.load("../../data/processed/"+lake_id+"/full.npy")
