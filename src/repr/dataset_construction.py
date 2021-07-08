68340d111417a6e0524dd1bf77047c5a60c4a6b5
Jared W <willa099@umn.edu>
2021-07-08 10:38:11 -0500

    move to remote

--- src/repr/dataset_construction.py
+++ src/repr/dataset_construction.py
@@ -10,9 +10,16 @@ import os
 site_ids = np.load("../../metadata/lakeset.npy")
 
 #load obs
-obs = pd.read
+obs = pd.read_csv("lake_surface_temp_obs.csv")
 
-
+#trim obs to sites
+pdb.set_trace()
+# 
 #create train/test split
-def createBinID(date):
-site_df[(site_df['Date'].str.contains('-12-')) | (site_df['Date'].str.contains('-01-')) | (site_df['Date'].str.contains('-02-'))]
+# def createBinID(date):
+
+# site_df[(site_df['Date'].str.contains('-12-')) | (site_df['Date'].str.contains('-01-')) | (site_df['Date'].str.contains('-02-'))]
+ #year since 1980 * 4 + season (winter1,spring2,summ3,fall4)
+
+
+ # for obs in obs
