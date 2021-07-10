fa096b439dc8b01f6b5ce0a2a37a3a4f9f9c1cfb
Jared W <willa099@umn.edu>
2021-07-10 17:02:31 -0500

    move to remote

--- src/repr/CTLSTM_area_depth_tst.py
+++ src/repr/CTLSTM_area_depth_tst.py
@@ -456,7 +456,7 @@ class Model(nn.Module):
 
 
 for r in range(n_runs):
-
+    print("starting run ",r)
     # lstm_net = myLSTM_Net(n_total_feats, n_hidden, batch_size)
     lstm_net = Model(input_size_dyn=n_features,input_size_stat=n_static_feats,hidden_size=n_hidden,no_static=True)
     save_path = '../../models/CTLSTM_area_depth_run'+str(r)
@@ -464,7 +464,7 @@ for r in range(n_runs):
     if not train[r]:
         load_path = save_path
         n_hidden = torch.load(load_path)['state_dict']['lstm.weight_hh'].shape[0]
-        lstm_net = Model(input_size_dyn=n_features,input_size_stat=n_static_feats,hidden_size=n_hidden)
+        lstm_net = Model(input_size_dyn=n_features,input_size_stat=n_static_feats,hidden_size=n_hidden,no_static=True)
         if use_gpu:
             lstm_net = lstm_net.cuda(0)
         pretrain_dict = torch.load(load_path)['state_dict']
