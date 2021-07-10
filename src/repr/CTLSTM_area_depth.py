4a11287443012f3ee6dde98eb9d9ab1a3d22a81c
Jared W <willa099@umn.edu>
2021-07-10 16:42:57 -0500

    move to remote

--- src/repr/CTLSTM_area_depth.py
+++ src/repr/CTLSTM_area_depth.py
@@ -119,36 +119,108 @@ see <https://opensource.org/licenses/Apache-2.0>
 
 #define LSTM model class
 class LSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, batch_size):
-        super(myLSTM_Net, self).__init__()
+    """Implementation of the standard LSTM.
+    TODO: Include ref and LaTeX equations
+    Parameters
+    ----------
+    input_size : int
+        Number of input features
+    hidden_size : int
+        Number of hidden/memory cells.
+    batch_first : bool, optional
+        If True, expects the batch inputs to be of shape [batch, seq, features] otherwise, the
+        shape has to be [seq, batch, features], by default True.
+    initial_forget_bias : int, optional
+        Value of the initial forget gate bias, by default 0
+    """
+
+    def __init__(self,
+                 input_size: int,
+                 hidden_size: int,
+                 batch_first: bool = True,
+                 initial_forget_bias: int = 0):
+        super(LSTM, self).__init__()
+
         self.input_size = input_size
         self.hidden_size = hidden_size
-        self.batch_size = batch_size
-        self.lstm = nn.LSTM(input_size = n_total_feats, hidden_size=hidden_size, batch_first=True,num_layers=num_layers,dropout=dropout) #batch_first=True?
-        self.out = nn.Linear(hidden_size, 1) #1?
-        self.hidden = self.init_hidden()
-        self.w_upper_to_lower = []
-        self.w_lower_to_upper = []
+        self.batch_first = batch_first
+        self.initial_forget_bias = initial_forget_bias
 
-    def init_hidden(self, batch_size=0):
-        # initialize both hidden layers
-        if batch_size == 0:
-            batch_size = self.batch_size
-        ret = (xavier_normal_(torch.empty(num_layers, batch_size, self.hidden_size)),
-                xavier_normal_(torch.empty(num_layers, batch_size, self.hidden_size)))
-        if use_gpu:
-            item0 = ret[0].cuda(non_blocking=True)
-            item1 = ret[1].cuda(non_blocking=True)
-            ret = (item0,item1)
-        return ret
+        # create tensors of learnable parameters
+        self.weight_ih = nn.Parameter(torch.FloatTensor(input_size, 4 * hidden_size))
+        self.weight_hh = nn.Parameter(torch.FloatTensor(hidden_size, 4 * hidden_size))
+        self.bias = nn.Parameter(torch.FloatTensor(4 * hidden_size))
 
-    def forward(self, x, hidden):
-        self.lstm.flatten_parameters()
-        x = x.float()
-        x, hidden = self.lstm(x, self.hidden)
-        self.hidden = hidden
-        x = self.out(x)
-        return x, hidden
+        # initialize parameters
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        """Initialize all learnable parameters of the LSTM"""
+        nn.init.orthogonal_(self.weight_ih.data)
+
+        weight_hh_data = torch.eye(self.hidden_size)
+        weight_hh_data = weight_hh_data.repeat(1, 4)
+        self.weight_hh.data = weight_hh_data
+        nn.init.constant_(self.bias.data, val=0)
+
+        if self.initial_forget_bias != 0:
+            self.bias.data[:self.hidden_size] = self.initial_forget_bias
+
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """[summary]
+        
+        Parameters
+        ----------
+        x : torch.Tensor
+            Tensor, containing a batch of input sequences. Format must match the specified format,
+            defined by the batch_first agrument.
+        Returns
+        -------
+        h_n : torch.Tensor
+            The hidden states of each time step of each sample in the batch.
+        c_n : torch.Tensor]
+            The cell states of each time step of each sample in the batch.
+        """
+        if self.batch_first:
+            x = x.transpose(0, 1)
+
+        seq_len, batch_size, _ = x.size()
+
+        h_0 = x.data.new(batch_size, self.hidden_size).zero_()
+        c_0 = x.data.new(batch_size, self.hidden_size).zero_()
+        h_x = (h_0, c_0)
+
+        # empty lists to temporally store all intermediate hidden/cell states
+        h_n, c_n = [], []
+
+        # expand bias vectors to batch size
+        bias_batch = (self.bias.unsqueeze(0).expand(batch_size, *self.bias.size()))
+
+        # perform forward steps over input sequence
+        for t in range(seq_len):
+            h_0, c_0 = h_x
+
+            # calculate gates
+            gates = (torch.addmm(bias_batch, h_0, self.weight_hh) + torch.mm(x[t], self.weight_ih))
+            f, i, o, g = gates.chunk(4, 1)
+
+            c_1 = torch.sigmoid(f) * c_0 + torch.sigmoid(i) * torch.tanh(g)
+            h_1 = torch.sigmoid(o) * torch.tanh(c_1)
+
+            # store intermediate hidden/cell state in list
+            h_n.append(h_1)
+            c_n.append(c_1)
+
+            h_x = (h_1, c_1)
+
+        h_n = torch.stack(h_n, 0)
+        c_n = torch.stack(c_n, 0)
+
+        if self.batch_first:
+            h_n = h_n.transpose(0, 1)
+            c_n = c_n.transpose(0, 1)
+
+        return h_n, c_n
 
 #method to calculate l1 norm of model
 def calculate_l1_loss(model):
