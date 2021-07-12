80ae16016fe8769e19595741ff7898833e7ca1ce
Jared W <willa099@umn.edu>
2021-07-12 13:52:58 -0500

    move to remote

--- src/oneoff/addCluster.py
+++ src/oneoff/addCluster.py
@@ -1,17 +1,18 @@
 import pandas as pd
+import pdb
 df_dr = pd.read_csv("../../../lake_conus_surface_temp_2021/metadata/lake_metadata.csv")
 df_old = pd.read_csv("../../metadata/surface_lake_metadata_041421_wCluster.csv")
-
+df_old.set_index('site_id',inplace=True)
 cluster = []
 
 ct = 0
 for site_id in df_dr['site_id'].values:
 	ct+=1
 	print(ct)
-	if df_old[df_old['site_id']==site_id]['cluster'].values.shape[0] == 0:
+	if df_old.loc[site_id]['cluster'].values.shape[0] == 0:
 		cluster.append(None)
 	else:
-		df_old[df_old['site_id']==site_id]['cluster'].values[0]
+		df_old.loc[site_id]['cluster'].values[0]
 
 pdb.set_trace()
 
