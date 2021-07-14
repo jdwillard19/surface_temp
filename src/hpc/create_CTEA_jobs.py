5ef5251c4a2c5f725a2eb7594ef0b5c570f54a0c
Jared W <willa099@umn.edu>
2021-07-14 12:58:30 -0500

    move to remote

--- src/hpc/create_CTEA_jobs.py
+++ src/hpc/create_CTEA_jobs.py
@@ -20,12 +20,12 @@ for name in rand_arr:
 
     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
     header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=CT_rand%s.out\n#SBATCH --error=CT_rand%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
-    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+    script = "source /home/kumarv/willa099/takeme_repr.sh\n" #cd to directory with training script
     script2 = "python CTLSTM_random.py %s"%(l)
     # script3 = "python singleModel_customSparse.py %s"%(l)
     all= "\n".join([header,script,script2])
-    sbatch = "\n".join(["sbatch job_%s_foldXGB_err.sh"%(l),sbatch])
-    with open('./jobs/job_{}_foldXGB_err.sh'.format(l), 'w') as output:
+    sbatch = "\n".join(["sbatch job_CTrand%s.sh"%(l),sbatch])
+    with open('./jobs/job_CTrand{}.sh'.format(l), 'w') as output:
         output.write(all)
 
 compile_job_path= './jobs/sbatch_CT_rand.sh'
@@ -46,12 +46,12 @@ for name in rand_arr:
 
     # if not os.path.exists("../../../models/single_lake_models/"+name+"/PGRNN_basic_normAll_pball"): 
     header = "#!/bin/bash -l\n#SBATCH --time=23:59:00\n#SBATCH --ntasks=8\n#SBATCH --mem=20g\n#SBATCH --mail-type=ALL\n#SBATCH --mail-user=willa099@umn.edu\n#SBATCH --output=EA_rand%s.out\n#SBATCH --error=EA_rand%s.err\n#SBATCH --gres=gpu:k40:2\n#SBATCH -p k40"%(l,l)
-    script = "source /home/kumarv/willa099/takeme_evaluate.sh\n" #cd to directory with training script
+    script = "source /home/kumarv/willa099/takeme_repr.sh\n" #cd to directory with training script
     script2 = "python EALSTM_random.py %s"%(l)
     # script3 = "python singleModel_customSparse.py %s"%(l)
     all= "\n".join([header,script,script2])
-    sbatch = "\n".join(["sbatch job_%s_foldXGB_err.sh"%(l),sbatch])
-    with open('./jobs/job_{}_foldXGB_err.sh'.format(l), 'w') as output2:
+    sbatch = "\n".join(["sbatch job_EArand%s.sh"%(l),sbatch])
+    with open('./jobs/job_EArand{}.sh'.format(l), 'w') as output2:
         output2.write(all)
 
 compile_job_path= './jobs/sbatch_EA_rand.sh'
